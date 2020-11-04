0e95afae1305e8bbe903f55a553939f468a2c0a4
Leigh Johnson <hi@leighjohnson.me>
2020-11-04 01:27:52 -0800

    initial commit

---
+++ print_nanny_webapp/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from print_nanny_webapp.users.models import User
+from print_nanny_webapp.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
