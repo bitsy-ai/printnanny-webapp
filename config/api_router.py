d481ff49090c2cf2602682ff6b549da60af9525d
Leigh Johnson <hi@leighjohnson.me>
2020-11-06 10:54:22 -0800

    fixup too users/me viewset

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from print_nanny_webapp.users.api.views import UserViewSet, MeViewSet
+from print_nanny_webapp.users.api.views import UserViewSet #, MeViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,7 +9,7 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
-router.register("me", MeViewSet, basename="me")
+#router.register("me", MeViewSet)
 
 
 app_name = "api"
--- print_nanny_webapp/users/api/views.py
+++ print_nanny_webapp/users/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateMode
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ViewSet
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .serializers import UserSerializer
 
@@ -20,17 +21,14 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     def get_queryset(self, *args, **kwargs):
         return self.queryset.filter(id=self.request.user.id)
 
-class MeViewSet(GenericViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-    name = "me"
-
-    @swagger_auto_schema(operation_id='get_me', operation_description="GET /me/")
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset()).first()
-        serializer = self.get_serializer(queryset)
+    @swagger_auto_schema(
+        operation_id='getMe', 
+        type=openapi.TYPE_OBJECT,
+        operation_description="GET /users/me/")
+    @action(detail=False, methods=["GET"])
+    def me(self, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
-    def get_queryset(self, *args, **kwargs):
-        return self.queryset.filter(id=self.request.user.id)
 
