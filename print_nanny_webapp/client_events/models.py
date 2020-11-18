fc22496287a78707d8a639818428aab9de5ea662
Leigh Johnson <hi@leighjohnson.me>
2020-11-18 01:31:42 -0800

    make print job non-nullable on PredictEvent

--- print_nanny_webapp/client_events/models.py
+++ print_nanny_webapp/client_events/models.py
@@ -103,7 +103,7 @@ class PredictEvent(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     files = models.ForeignKey(PredictEventFile, on_delete=models.CASCADE)
-    print_job = models.ForeignKey(PrintJob, null=True, on_delete=models.SET_NULL)
+    print_job = models.ForeignKey(PrintJob, on_delete=models.CASCADE)
 
     plugin_version = models.CharField(max_length=30)
     octoprint_version = models.CharField(max_length=30)
