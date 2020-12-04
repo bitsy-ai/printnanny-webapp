a71b25aba0ba85479e84d6e21d87fc265df4da47
Leigh Johnson <hi@leighjohnson.me>
2020-12-03 23:41:56 -0800

    fix image tagging in push script

--- k8s/push.sh
+++ k8s/push.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-gcloud docker push print_nanny_webapp_production_django:latest
+docker tag print_nanny_webapp_production_django:latest us.gcr.io/print-nanny/print_nanny_webapp_production_django
+
+docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django
 kubectl apply configmap django --from-file=.envs/.production/.django
 kubectl apply configmap postgres --from-file=.envs/.production/.postgres
 kubectl apply k8s/production.yml
