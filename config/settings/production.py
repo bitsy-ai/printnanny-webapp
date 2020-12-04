eeae365837d591ed5a73672fa87ed7a85487ad0c
Leigh Johnson <hi@leighjohnson.me>
2020-12-03 20:23:03 -0800

    fixup dependencies, settings, .env files for prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -62,12 +62,6 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
     "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
 )
 
-# STORAGES
-# ------------------------------------------------------------------------------
-# https://django-storages.readthedocs.io/en/latest/#installation
-INSTALLED_APPS += ["storages"]  # noqa F405
-GS_BUCKET_NAME = env("DJANGO_GCP_STORAGE_BUCKET_NAME")
-# GS_DEFAULT_ACL = "publicRead"
 # STATIC
 # ------------------------
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
