777efc224d4b83ef36d740e47510fd24ad495b27
Leigh Johnson <hi@leighjohnson.me>
2020-12-04 21:05:52 -0800

    run compressor

--- k8s/push.sh
+++ k8s/push.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-docker tag print_nanny_webapp_production_django:latest us.gcr.io/print-nanny/print_nanny_webapp_production_django
+docker tag print_nanny_webapp_production_django:latest \
+    us.gcr.io/print-nanny/print_nanny_webapp_production_django:latest
 
-docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django
-kubectl apply configmap django --from-file=.envs/.production/.django
-kubectl apply configmap postgres --from-file=.envs/.production/.postgres
-kubectl apply k8s/production.yml
+docker tag print_nanny_webapp_redis:latest \
+    us.gcr.io/print-nanny/print_nanny_webapp_redis:latest
+
+docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django:latest
+docker push us.gcr.io/print-nanny/print_nanny_webapp_redis:latest
+
+#kubectl create configmap webapp --from-file=k8s/configmap.yml --dry-run -o yaml | kubectl apply -f -
+#kubectl create configmap postgres --from-file=.envs/.production/.postgres --dry-run -o yaml | kubectl apply -f -
+kubectl replace -f k8s/configmap.yml
+kubectl apply -f k8s/production.yml
+
+
+# gcloud iam service-accounts add-iam-policy-binding \
+#   --role roles/iam.workloadIdentityUser \
+#   --member "serviceAccount:print-nanny.svc.id.goog[default/sql-webapp]" \
+#   sql-webapp@print-nanny.iam.gserviceaccount.com
+
+
+# kubectl annotate serviceaccount \
+#    sql-webapp \
+#    iam.gke.io/gcp-service-account=sql-webapp@print-nanny.iam.gserviceaccount.com
