b904232f9b206241f56b47a5f36838093e29ffc6
Leigh Johnson <hi@leighjohnson.me>
2020-12-06 21:18:01 -0800

    first pass on progress alert task

--- print_nanny_webapp/remote_control/models.py
+++ print_nanny_webapp/remote_control/models.py
@@ -79,3 +79,8 @@ class PrintJob(models.Model):
     last_seen = models.DateTimeField(auto_now=True)
 
     progress = models.IntegerField(default=0)
+
+
+    @property
+    def filename(self):
+        return self.gcode_file.file.name
