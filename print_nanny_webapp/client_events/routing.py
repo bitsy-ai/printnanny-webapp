1d2525646f1f955a5a5ee48f1d1582d241a61a2f
Leigh Johnson <hi@leighjohnson.me>
2020-12-12 14:20:00 -0800

    use urls.path instead of re url pattern

--- print_nanny_webapp/client_events/routing.py
+++ print_nanny_webapp/client_events/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .consumers import PredictEventConsumer
 
 websocket_urlpatterns = [
-    path(r'ws/predict/(?P<print_job_id>\d+)/$', PredictEventConsumer.as_asgi()),
+    path('ws/predict/<print_job_id>', PredictEventConsumer.as_asgi()),
 ]
