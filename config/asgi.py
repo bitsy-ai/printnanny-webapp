8c53a467c51e16ff52dc01bc0a54ecb4ed121926
Leigh Johnson <hi@leighjohnson.me>
2020-12-12 17:43:27 -0800

    checking working predict channel

--- config/asgi.py
+++ config/asgi.py
@@ -32,6 +32,10 @@ django_application = get_asgi_application()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import print_nanny_webapp.client_events.routing
+from rest_framework.authtoken.models import Token
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import AnonymousUser
+
 
 async def application(scope, receive, send):
     if scope["type"] == "http":
@@ -41,11 +45,51 @@ async def application(scope, receive, send):
     else:
         raise NotImplementedError(f"Unknown scope type {scope['type']}")
 
+@database_sync_to_async
+def get_user(headers):
+    try:
+        token_name, token_key = headers[b'authorization'].decode().split()
+        if token_name == 'Bearer':
+            token = Token.objects.get(key=token_key)
+            return token.user
+    except Token.DoesNotExist:
+        return AnonymousUser()
+
+##
+# https://stackoverflow.com/questions/60009296/django-3-0-channels-asgi-tokenauthmiddleware
+##
+class TokenAuthMiddleware:
+    def __init__(self, inner):
+        self.inner = inner
+
+    async def __call__(self, scope, receive, send):
+        headers = dict(scope['headers'])
+        if b'authorization' in headers:
+            scope['user'] = await get_user(headers)
+        return await self.inner(scope, receive, send)
+
+class TokenAuthMiddlewareInstance:
+    """
+    Yeah, this is black magic:
+    https://github.com/django/channels/issues/1399
+    """
+    def __init__(self, scope, middleware):
+        self.middleware = middleware
+        self.scope = dict(scope)
+        self.inner = self.middleware.inner
+
+    async def __call__(self, receive, send):
+        headers = dict(self.scope['headers'])
+        if b'authorization' in headers:
+            self.scope['user'] = await get_user(headers)
+        inner = self.inner(self.scope)
+        return await inner(receive, send)
+
+
+TokenAuthMiddlewareStack = lambda inner: TokenAuthMiddleware(AuthMiddlewareStack(inner))
+
 application = ProtocolTypeRouter({
   "http": django_application,
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            print_nanny_webapp.client_events.routing.websocket_urlpatterns
-        )
-    ),
+  "websocket": TokenAuthMiddlewareStack(
+      URLRouter(print_nanny_webapp.client_events.routing.websocket_urlpatterns))
 })
--- print_nanny_webapp/client_events/consumers.py
+++ print_nanny_webapp/client_events/consumers.py
@@ -1,31 +1,57 @@
 import json
-
+import logging
+import base64
+import hashlib
 from .models import PredictEvent, PredictEventFile
 from channels.generic.websocket import WebsocketConsumer
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.apps import apps
+from django.contrib.auth import get_user_model
+
+
+logger = logging.getLogger(__name__)
+
+PrintJob = apps.get_model('remote_control', 'PrintJob')
+User = get_user_model()
 
 class PredictEventConsumer(WebsocketConsumer):
 
     def connect(self):
         self.accept()
+        self.user = self.scope['user']
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
         data = json.loads(text_data)
-        print_job_id = self.scope['url_route']['kwargs']['print_job_id']
-        files = PredictEventFile.objects.create(
-            annotated_image=SimpleUploadedFile('annotated_image.jpg', data['annotated_image']),
-            hash=data['file'],
-            original_image=SimpleUploadedFile('original_image.jpg', data['original_image']),
-        )
 
-        predict_event = PredictEvent.objects.create(
-            dt=data['ts'],
-            predict_data=data['predict_data'],
-            user=self.request.user,
-            files=files,
-            print_job=print_job_id
-        )
+        if data.get('msg') == "ping":
+            return self.send(text_data="pong")
 
+        elif data.get('msg') == "predict":
+            print_job_id = self.scope['url_route']['kwargs']['print_job_id']
+
+            original_img = base64.b64decode(data['original_image'])
+            imghash =  hashlib.md5(original_img).hexdigest()
+
+            files = PredictEventFile.objects.create(
+                annotated_image=SimpleUploadedFile('annotated_image.jpg', base64.b64decode(data['annotated_image'])),
+                hash=imghash,
+                original_image=SimpleUploadedFile('original_image.jpg', base64.b64decode(data['original_image'])),
+            )
+
+            job = PrintJob(id=print_job_id)
+
+            if job.user_id ==  self.user.id:
+
+                predict_event = PredictEvent.objects.create(
+                    dt=data['ts'],
+                    predict_data=data['predict_data'],
+                    user=self.user,
+                    files=files,
+                    print_job=job
+                )
+
+            else:
+                logging.warning(f'user {self.user} tried to publish event to job {job.id}')
