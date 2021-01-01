07229c4d7b42b4342f69de8e47bb9fd84e27aef7
Leigh Johnson <leigh@bitsy.ai>
2020-12-31 23:06:09 -0800

    Mqtt telemetry (#12)
    * add google root certs
    
    * add aiohttp to rest client requirements
    
    * add pytest runner requirements
    
    * check in gateway configurations (even tho gateway is likely not required
    
    * rm stock test
    
    * check in rough gateway pass; this code is not necessary
    
    * add ObjectDetectEventImage model
    
    * rename tracking -> telemetry, add PrintProgress
    
    * lint
    
    * Bump version: 0.1.10 → 0.1.11
    
    * lint
    
    * first pass on get-and-update-key or create flow for octoprint devices
    
    * Bump version: 0.1.11 → 0.1.12
    
    * move create -> update_or_create in OctoPrintDevice viewset
    
    * Bump version: 0.1.12 → 0.1.13
    
    * missing s
    
    * Bump version: 0.1.13 → 0.1.14
    
    * check in mqtt server/client model for channels, not currently used
    
    * add first pass on pubsub subscriber, fix bug where public key was being sent as private kye
    
    * fixup update_or_create method
    
    * Bump version: 0.1.14 → 0.1.15
    
    * fix swagger tag
    
    * Bump version: 0.1.15 → 0.1.16
    
    * make private key write-only
    
    * Bump version: 0.1.16 → 0.1.17
    
    * using exclude instead of fieldset
    
    * Bump version: 0.1.17 → 0.1.18
    
    * add a separate key erializer
    
    * Bump version: 0.1.18 → 0.1.19
    
    * include fields
    
    * fix instance missing in serializer data, add events to OctoPrint events model
    
    * add PrintJobEvent class
    
    * add PrintJobEvent class, fixup pubsub subscriber
    
    * log debug level

--- print_nanny_webapp/client_events/consumers.py
+++ print_nanny_webapp/client_events/consumers.py
@@ -7,6 +7,7 @@ from django.apps import apps
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from asgiref.sync import async_to_sync
+from django.core.files.base import ContentFile
 
 from print_nanny_webapp.utils.prometheus_metrics import (
     annotated_ws_publisher_connected_metric,
@@ -17,6 +18,8 @@ from print_nanny_webapp.utils.prometheus_metrics import (
 logger = logging.getLogger(__name__)
 
 PrintJob = apps.get_model("remote_control", "PrintJob")
+ObjectDetectEventImage = apps.get_model("client_events", "ObjectDetectEventImage")
+
 User = get_user_model()
 
 
@@ -28,7 +31,7 @@ class VideoConsumer(WebsocketConsumer):
             f"video_{self.user.id}", self.channel_name
         )
 
-        annotated_ws_consumer_connected_metric.inc()
+        annotated_ws_consumer_connected.inc()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
@@ -37,7 +40,7 @@ class VideoConsumer(WebsocketConsumer):
 
         super().disconnect(close_code)
 
-        annotated_ws_consumer_connected_metric.dec()
+        annotated_ws_consumer_connected.dec()
 
     def video_frame(self, message):
         logging.info("Received video message")
@@ -49,12 +52,12 @@ class ObjectDetectEventConsumer(WebsocketConsumer):
         self.accept()
         self.user = self.scope["user"]
 
-        annotated_image_ws_connected_metric.inc()
+        annotated_ws_publisher_connected_metric.inc()
 
     def disconnect(self, close_code):
 
         super().disconnect(close_code)
-        annotated_image_ws_connected_metric.dec()
+        annotated_ws_publisher_connected_metric.dec()
 
     def receive(self, text_data):
         data = json.loads(text_data)
@@ -70,31 +73,16 @@ class ObjectDetectEventConsumer(WebsocketConsumer):
                 {"type": "video.frame", "data": data["annotated_image"]},
             )
 
-            # filelike = io.BytesIO()
-            # writer = DataFileWriter(filelike, DatumWriter(), avro_schema)
-            # writer.append({
-            #     'created_ts': data['ts'].time,
-            #     'predict_session_id': self.scope.predict_session_id,
-            #     'user_id': self.scope.user.id,
-            #     'print_job_id': print_job_id,
-            #     'object_detect_data': data["predict_data"],
-            #     'annotated_image': base64.b64decode(data["annotated_image"]),
-            #     'original_image': base64.b64decode(data["original_image"])
-            # })
+            original_image = ContentFile(data["original_image"])
+            annotated_image = ContentFile(data["annotated_image"])
 
-            # future = publisher.publish(
-            #     topic_path,
-            #     filelike.read(),
-            #     print_job_id=print_job_id,
-            #     user_id=self.scope.user.id,
-            #     predict_session_id=self.scope.predict_session.id
-            # )
-            # future.result()
+            event = ObjectDetectEventImage.objects.create(
+                created_dt=data["ts"],
+                uuid=data["uuid"],
+                user=self.scope.user,
+                print_job=job,
+            )
 
-            # event = ObjectDetectEvent.objects.create(
-            #     created_dt=data["ts"],
-            #     predict_data=data["predict_data"],
-            #     files=files,
-            #     print_job=job,
-            #     predict_session=self.predict_session,
-            # )
+            event.annotated_image.save(f"{event.uuid}.jpg", annotated_image)
+            event.original_image.save(f"{event.uuid}.jpg", original_image)
+            event.save()
--- print_nanny_webapp/client_events/routing.py
+++ print_nanny_webapp/client_events/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .consumers import ObjectDetectEventConsumer, VideoConsumer
 
 websocket_urlpatterns = [
-    path("ws/annotated-image/", ObjectDetectEventConsumer.as_asgi()),
+    path("ws/images/", ObjectDetectEventConsumer.as_asgi()),
     path("ws/video/", VideoConsumer.as_asgi()),
 ]
