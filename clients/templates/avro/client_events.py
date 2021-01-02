f94c305b60f25549b3ef7043ba39849712461afb
Leigh Johnson <hi@leighjohnson.me>
2021-01-01 20:28:49 -0800

    try out fastavro

--- clients/templates/avro/client_events.py
+++ clients/templates/avro/client_events.py
@@ -1,12 +1,11 @@
-import avro.schema
 import io
-from avro.io import DatumReader, DatumWriter, BinaryEncoder
+from fastavro import schemaless_writer, reader, parse_schema
 from os import path
 
 basepath = path.dirname(__file__)
 
 filename = '{basepath}/client_events.avsc'.format(basepath=basepath)
-schema = avro.schema.Parse(open(filename, "rb").read())
+schema = parse_schema(open(filename, "rb").read())
 
 class ObjectDetectEventSerializer:
 
@@ -14,29 +13,7 @@ class ObjectDetectEventSerializer:
         self.writer = DatumWriter(schema)
         self.reader = DatumReader(schema)
 
-    
-    def encode_bytes(self, client_events=[]):
+    def schemaless_encode(self, client_event):
         bytes_writer = io.BytesIO()
-        encoder = BinaryEncoder(bytes_writer)
-        for client_event in client_events:
-            self.writer.write(client_event, encoder)
+        schemaless_writer(bytes_writer, schema, client_event)
         return bytes_writer.get_value()
-
-    def encode_bytes_io(self, client_events=[]):
-        bytes_writer = io.BytesIO()
-        encoder = BinaryEncoder(bytes_writer)
-        for client_event in client_events:
-            self.writer.write(client_event, encoder)
-        return bytes_writer
-    
-    def decode_bytes(self, raw_bytes):
-        bytes_reader = io.BytesIO(raw_bytes)
-        decoder = BinaryDecoder(bytes_reader)
-        client_events = []
-        while bytes_reader.tell() < len(raw_bytes):
-            client_events.append(self.reader.read(decoder))
-        
-        if len(client_events) == 1:
-            return client_events[0]
-        else:
-            return client_events
