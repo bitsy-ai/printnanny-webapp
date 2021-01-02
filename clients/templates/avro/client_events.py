4c2722fb63c1dd2736a8b2d70aee2d0e07b02040
Leigh Johnson <hi@leighjohnson.me>
2021-01-01 20:52:05 -0800

    rename avro aschema again

--- clients/templates/avro/client_events.py
+++ clients/templates/avro/client_events.py
@@ -1,19 +1,27 @@
 import io
-from fastavro import schemaless_writer, reader, load_schema
+from fastavro import writer, reader, schemaless_writer
+from fastavro.schema import load_schema
 from os import path
 
 basepath = path.dirname(__file__)
 
-filename = '{basepath}/client_events.avsc'.format(basepath=basepath)
+filename = '{basepath}/ObjectDetectEvent.avsc'.format(basepath=basepath)
 schema = load_schema(filename)
 
 class ObjectDetectEventSerializer:
-
-    def __init__(self):
-        self.writer = DatumWriter(schema)
-        self.reader = DatumReader(schema)
 
     def schemaless_encode(self, client_event):
         bytes_writer = io.BytesIO()
         schemaless_writer(bytes_writer, schema, client_event)
         return bytes_writer.get_value()
+    
+    def encode(self, client_event):
+        fo = BytesIO()
+        writer(fo, schema, client_event)
+        return fo.get_value()
+    
+    def decode(self, raw_bytes):
+        fo = BytesIO()
+        writer(fo, schema, raw_bytes)
+        fo.seek(0)
+        return record for record in reader(fo)    
