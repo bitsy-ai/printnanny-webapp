e5072b830459851a0df8db4ba6febf7b50de4fee
Leigh Johnson <hi@leighjohnson.me>
2021-01-03 13:03:19 -0800

    subscribe / publish to video channel layer by device serial

--- print_nanny_webapp/client_events/routing.py
+++ print_nanny_webapp/client_events/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .consumers import ObjectDetectEventConsumer, VideoConsumer
 
 websocket_urlpatterns = [
-    path("ws/images/", ObjectDetectEventConsumer.as_asgi()),
-    path("ws/video/", VideoConsumer.as_asgi()),
+    path("ws/images/<str:serial>/", ObjectDetectEventConsumer.as_asgi()),
+    path("ws/video/<str:serial>/", VideoConsumer.as_asgi()),
 ]
