2023c52c2080c50b1fc8d0aef6c67996eb0ad683
Leigh Johnson <hi@leighjohnson.me>
2021-01-09 20:26:07 -0800

    add custom Honeycomb middleware, ignore health check and static file requests

---
+++ print_nanny_webapp/middleware/honeycomb.py
@@ -0,0 +1,25 @@
+import contextlib
+from django.db import connections
+import logging
+from beeline.middleware.django import HoneyMiddlewareBase, HoneyDBWrapper
+
+
+class HoneyMiddlewareIgnoreHealthCheck(HoneyMiddlewareBase):
+
+    ignored = ['health', 'static']
+    
+    def __call__(self, request):
+
+        if any([ignored_path in request.get_full_path_info() for ignored_path in self.ignored]):
+            return self.get_response(request)
+        try:
+            db_wrapper = HoneyDBWrapper()
+            # db instrumentation is only present in Django > 2.0
+            with contextlib.ExitStack() as stack:
+                for connection in connections.all():
+                    stack.enter_context(connection.execute_wrapper(db_wrapper))
+                response = self.create_http_event(request)
+        except AttributeError:
+            response = self.create_http_event(request)
+
+        return response
