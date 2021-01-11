b22ca904facc3fa8ea5de96608f6d3003f27a7fd
Leigh Johnson <hi@leighjohnson.me>
2021-01-10 17:09:33 -0800

    fixup octoprint device list / printer profile list state, start adding eventstream

--- print_nanny_webapp/client_events/routing.py
+++ print_nanny_webapp/client_events/routing.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
+import django_eventstream
+from channels.routing import URLRouter
 from .consumers import ObjectDetectEventConsumer, VideoConsumer
 
 websocket_urlpatterns = [
+    path("ws/remote-control/", URLRouter(django_eventstream.routing.urlpatterns), {'channels': ['remote-control-{device-id}']} ),
     path("ws/<int:device_id>/video/upload/", ObjectDetectEventConsumer.as_asgi()),
     path("ws/<int:device_id>/video/download/", VideoConsumer.as_asgi()),
 ]
