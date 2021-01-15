27a6fc3c42fff874dce728ce54c63b0ce131c191
Leigh Johnson <hi@leighjohnson.me>
2021-01-14 19:30:28 -0800

    check in base generated files before attempting to reconfigure webpack

--- print_nanny_vue/src/main.js
+++ print_nanny_vue/src/main.js
@@ -1,14 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+/* eslint-disable no-new */
+const store = new Vuex.Store({
+  state: {
+    alerts: []
+  },
+  mutations: {
+    dismissAlert (state) {
+      state.count++
+    }
+  }
+})
+
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#alerts',
   router,
   components: { App },
   template: '<App/>'
