eb1f6b1f95cb7f1def53c0340fbb79085f2d74b0
Leigh Johnson <hi@leighjohnson.me>
2021-02-01 13:03:35 -0800

    update push script to set image on celery-worker and octoprint-events deployments

--- k8s/push.sh
+++ k8s/push.sh
@@ -12,29 +12,10 @@ docker tag print_nanny_webapp_production_django:latest \
 docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django:latest
 docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD)
 
-##
-# redis
-##
 
-docker tag print_nanny_webapp_redis:latest \
-    us.gcr.io/print-nanny/print_nanny_webapp_redis:latest
-docker push us.gcr.io/print-nanny/print_nanny_webapp_redis:latest
-
-#kubectl create configmap webapp --from-file=k8s/configmap.yml --dry-run -o yaml | kubectl apply -f -
-#kubectl create configmap postgres --from-file=.envs/.production/.postgres --dry-run -o yaml | kubectl apply -f -
 kubectl apply -f k8s/configmap.yml
 kubectl apply -f k8s/production.yml
 
 kubectl set image deployment/django django=us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD) --record
-
-
-
-# gcloud iam service-accounts add-iam-policy-binding \
-#   --role roles/iam.workloadIdentityUser \
-#   --member "serviceAccount:print-nanny.svc.id.goog[default/sql-webapp]" \
-#   sql-webapp@print-nanny.iam.gserviceaccount.com
-
-
-# kubectl annotate serviceaccount \
-#    sql-webapp \
-#    iam.gke.io/gcp-service-account=sql-webapp@print-nanny.iam.gserviceaccount.com
+kubectl set image deployment/celery-worker celery-worker=us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD) --record
+kubectl set image deployment/octoprint-events octoprint-events=us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD) --record
