cbdc0516cdd1358583107294ebcdd9d1b6660ce6
Leigh Johnson <hi@leighjohnson.me>
2021-02-02 00:01:38 -0800

    add storage class with public ACL ; use public acl storage class in snapshot image field

--- print_nanny_webapp/remote_control/api/views.py
+++ print_nanny_webapp/remote_control/api/views.py
@@ -260,7 +260,7 @@ class RemoteControlSnapshotViewSet(
             # https://github.com/aio-libs/aiohttp/issues/3652
             # octoprint_device is accepted as a string and deserialized to an integer
             instance = serializer.create(serializer.validated_data)
-            response_serializer = RemoteControlSnapshotCreateResponseSerializer(instance=instance)
+            response_serializer = RemoteControlSnapshotCreateResponseSerializer(instance=instance, context={'request': self.request})
             return Response(response_serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- print_nanny_webapp/utils/storages.py
+++ print_nanny_webapp/utils/storages.py
@@ -1,6 +1,8 @@
 from storages.backends.gcloud import GoogleCloudStorage
-from storages.utils import safe_join
-
+from storages.utils import (
+    check_location, clean_name, get_available_overwrite_name, safe_join,
+    setting,
+)
 
 class StaticRootGoogleCloudStorage(GoogleCloudStorage):
     location = "static"
@@ -23,3 +25,73 @@ class MediaRootGoogleCloudStorage(GoogleCloudStorage):
             return safe_join(self.location, name)
         except ValueError:
             pass
+    def url(self, name, version="v4"):
+        """
+        Return public url or a signed url for the Blob.
+        This DOES NOT check for existance of Blob - that makes codes too slow
+        for many use cases.
+        """
+        name = self._normalize_name(clean_name(name))
+        blob = self.bucket.blob(name)
+        # no_signed_url = (
+        #     self.default_acl == 'publicRead' or not self.querystring_auth)
+
+        # if not self.custom_endpoint and no_signed_url:
+        #     return blob.public_url
+        # elif no_signed_url:
+        #     return '{storage_base_url}/{quoted_name}'.format(
+        #         storage_base_url=self.custom_endpoint,
+        #         quoted_name=_quote(name, safe=b"/~"),
+        #     )
+        # elif not self.custom_endpoint:
+        return blob.generate_signed_url(self.expiration, version=version)
+        # else:
+        #     return blob.generate_signed_url(
+        #         expiration=self.expiration,
+        #         api_access_endpoint=self.custom_endpoint,
+        #         version=version
+        #     )
+
+
+class PublicGoogleCloudStorage(GoogleCloudStorage):
+    location = "public"
+    file_overwrite = True
+    default_acl = "publicRead"
+
+    def _normalize_name(self, name):
+        """
+        Normalizes the name so that paths like /path/to/ignored/../something.txt
+        and ./file.txt work.  Note that clean_name adds ./ to some paths so
+        they need to be fixed here. We check to make sure that the path pointed
+        to is not outside the directory specified by the LOCATION setting.
+        """
+        try:
+            return safe_join(self.location, name)
+        except ValueError:
+            pass
+    def url(self, name, version="v4"):
+        """
+        Return public url or a signed url for the Blob.
+        This DOES NOT check for existance of Blob - that makes codes too slow
+        for many use cases.
+        """
+        name = self._normalize_name(clean_name(name))
+        blob = self.bucket.blob(name)
+        # no_signed_url = (
+        #     self.default_acl == 'publicRead' or not self.querystring_auth)
+
+        # if not self.custom_endpoint and no_signed_url:
+        #     return blob.public_url
+        # elif no_signed_url:
+        #     return '{storage_base_url}/{quoted_name}'.format(
+        #         storage_base_url=self.custom_endpoint,
+        #         quoted_name=_quote(name, safe=b"/~"),
+        #     )
+        # elif not self.custom_endpoint:
+        return blob.generate_signed_url(self.expiration, version=version)
+        # else:
+        #     return blob.generate_signed_url(
+        #         expiration=self.expiration,
+        #         api_access_endpoint=self.custom_endpoint,
+        #         version=version
+        #     )
