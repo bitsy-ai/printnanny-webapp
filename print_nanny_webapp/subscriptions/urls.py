43926686b164276d916731b73f8fa1e92bf7ad4a
Dimitris Zervas <dzervas@dzervas.gr>
2021-03-09 03:09:18 +0200

    Subscriptions are working

--- print_nanny_webapp/subscriptions/urls.py
+++ print_nanny_webapp/subscriptions/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from print_nanny_webapp.subscriptions.views import SubscriptionsSuccessView, SubscriptionsListView, subscriptions_payment_view_create
+from print_nanny_webapp.subscriptions.views import SubscriptionsSuccessView, SubscriptionsListView, subscriptions_payment_intent_view_create
 
 app_name = "subscriptions"
 
@@ -8,5 +8,5 @@ urlpatterns = [
     path(r"^stripe/", include("djstripe.urls", namespace="djstripe")),
     path("", SubscriptionsListView.as_view(), name="list"),
     path("success/<id>", SubscriptionsSuccessView.as_view(), name="success"),
-    path("payment", subscriptions_payment_view_create, name="payment"),
+    path("payment_intent", subscriptions_payment_intent_view_create, name="payment_intent"),
 ]
--- print_nanny_webapp/subscriptions/views.py
+++ print_nanny_webapp/subscriptions/views.py
@@ -24,7 +24,7 @@ class SubscriptionsListView(DashboardView):
         ctx["STRIPE_PUBLIC_KEY"] = settings.STRIPE_PUBLIC_KEY
         return ctx
 
-    def payment_form_valid(self, form):
+    def form_valid(self, form):
         print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         stripe_source = form.cleaned_data["stripe_source"]
         plan = form.cleaned_data["plan"]
@@ -72,26 +72,44 @@ class SubscriptionsSuccessView(DashboardView):
     context_object_name = "subscription"
 
 
-def subscriptions_payment_view_create(request: HttpRequest):
-    if not request.POST:
+# TODO: Maybe this needs to be a normal class? Maybe throw it above?
+# Might not be possible due to "requires_action" but could use REST API thing
+# TODO: Use crispy form
+def subscriptions_payment_intent_view_create(request: HttpRequest):
+    if request.method != "POST":
         ctx = {"STRIPE_PUBLIC_KEY": djstripe.settings.STRIPE_PUBLIC_KEY}
         return TemplateResponse(request, SubscriptionsListView.template_name, ctx)
 
     intent = None
+    data = json.loads(request.body)
+    # TODO: Put this in the form
+    data["plan"] = djstripe.models.Plan.objects.first().id
     try:
-        if request.POST.get("payment_method_id", None):
-            # Create the PaymentIntent
-            intent = stripe.PaymentIntent.create(
-                payment_method=request.body.get("payment_method_id"),
-                amount=500,
-                currency="usd",
-                confirmation_method="manual",
-                confirm=True,
+        # TODO: This is just ugly
+        if "payment_method_id" in data:
+            customer, created = djstripe.models.Customer.get_or_create(subscriber=request.user)
+            customer.add_payment_method(data["payment_method_id"])
+
+            # Using the Stripe API, create a subscription for this customer,
+            # using the customer's default payment source
+            stripe_subscription = stripe.Subscription.create(
+                customer=customer.id,
+                items=[{"plan": data["plan"]}],
+                collection_method="charge_automatically",
+                # tax_percent=15,
                 api_key=djstripe.settings.STRIPE_SECRET_KEY,
             )
-        elif request.POST.get("payment_intent_id", None):
+
+            # Sync the Stripe API return data to the database,
+            # this way we don't need to wait for a webhook-triggered sync
+            subscription = djstripe.models.Subscription.sync_from_stripe_data(
+                stripe_subscription
+            )
+
+            intent = stripe_subscription
+        elif "payment_intent_id" in data:
             intent = stripe.PaymentIntent.confirm(
-                request.body.get("payment_intent_id"),
+                data["payment_intent_id"],
                 api_key=djstripe.settings.STRIPE_SECRET_KEY,
             )
     except stripe.error.CardError as e:
@@ -101,6 +119,7 @@ def subscriptions_payment_view_create(request: HttpRequest):
             return_data[0], content_type="application/json", status=return_data[1]
         )
 
+    # For more: https://stripe.com/docs/payments/intents#intent-statuses
     if intent.status == "requires_action" and intent.next_action.type == "use_stripe_sdk":
         # Tell the client to handle the action
         return_data = (
@@ -112,11 +131,12 @@ def subscriptions_payment_view_create(request: HttpRequest):
             ),
             200,
         )
-    elif intent.status == "succeeded":
+    elif intent.status == "succeeded" or intent.status == "active":
         # The payment did not need any additional actions and completed!
         # Handle post-payment fulfillment
         return_data = json.dumps({"success": True}), 200
     else:
+        print(intent.status)
         # Invalid status
         return_data = json.dumps({"error": "Invalid PaymentIntent status"}), 500
     return HttpResponse(return_data[0], content_type="application/json", status=return_data[1])
