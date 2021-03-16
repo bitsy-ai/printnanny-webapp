32f6028f64ec7a1e0458efd00b548943ddf58d79
Dimitris Zervas <dzervas@dzervas.gr>
2021-03-17 01:12:59 +0200

    Add text email templates

--- print_nanny_webapp/subscriptions/apps.py
+++ print_nanny_webapp/subscriptions/apps.py
@@ -1,61 +1,86 @@
 from anymail.message import AnymailMessage
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 from django.template.loader import render_to_string
-from django.urls import reverse
 from djstripe import webhooks
 
-# from .models import User
-
-# import djstripe.models
 import logging
 
 logger = logging.getLogger(__name__)
 
 @webhooks.handler("customer.subscription.trial_will_end")
-def subscriptions_trial_will_end(event, **kwargs):
-    print("*** subscription trial ends! ***")
-
-@webhooks.handler("customer.subscription.payment_failed")
-def subscriptions_payment_failed(event, **kwargs):
-    print("*** subscription payment failed! ***")
-
-@webhooks.handler("customer.subscription.deleted")
-def subscriptions_cancelled(event):
-    # user = User.objects.get(email=event.customer.email)
+def subscriptions_trial_will_end(event):
     merge_data = {
-        "REPORT_URL": reverse("subscriptions:list"),
-        "FIRST_NAME": event.customer.name or "Maker",
+        "FIRST_NAME": event.account_name or "Maker",
     }
 
-    text_body = render_to_string("email/timelapse_alert_body.txt", merge_data)
-    html_body = render_to_string("email/timelapse_alert_body.html", merge_data)
-    subject = render_to_string("email/timelapse_alert_subject.txt", merge_data)
+    text_body = render_to_string("email/subscriptions_trial_ending_body.txt", merge_data)
+    # html_body = render_to_string("email/subscriptions_trial_ending_body.html", merge_data)
+    subject = render_to_string("email/subscriptions_trial_ending_subject.txt", merge_data)
 
     message = AnymailMessage(
         subject=subject,
         body=text_body,
         to=[event.customer.email],
-        tags=["default-print-alert"],  # Anymail extra in constructor
     )
-    message.attach_alternative(html_body, "text/html")
+    # message.attach_alternative(html_body, "text/html")
     message.send()
 
 @webhooks.handler("subscription_schedule.expiring")
-def subscriptions_subscription_expiring(event, **kwargs):
-    print("*** subscription expiring! ***")
+def subscriptions_subscription_expiring(event):
+    merge_data = {
+        "FIRST_NAME": event.account_name or "Maker",
+    }
 
-@webhooks.handler("invoice.upcoming")
-def subscriptions_trial_will_end(event, **kwargs):
-    print("*** invoice upcoming! ***")
+    text_body = render_to_string("email/subscriptions_subscription_expiring_body.txt", merge_data)
+    # html_body = render_to_string("email/subscriptions_subscription_expiring_body.html", merge_data)
+    subject = render_to_string("email/subscriptions_subscription_expiring_subject.txt", merge_data)
+
+    message = AnymailMessage(
+        subject=subject,
+        body=text_body,
+        to=[event.customer.email],
+    )
+    # message.attach_alternative(html_body, "text/html")
+    message.send()
+
+@webhooks.handler("charge.failed")
+def subscriptions_payment_failed(event):
+    merge_data = {
+        "FIRST_NAME": event.account_name or "Maker",
+    }
+
+    text_body = render_to_string("email/subscriptions_payment_failed_body.txt", merge_data)
+    # html_body = render_to_string("email/subscriptions_subscription_expiring_body.html", merge_data)
+    subject = render_to_string("email/subscriptions_payment_failed_subject.txt", merge_data)
+
+    message = AnymailMessage(
+        subject=subject,
+        body=text_body,
+        to=[event.customer.email],
+    )
+    # message.attach_alternative(html_body, "text/html")
+    message.send()
 
 # Payment action required is handled during the payment step inside the view
 # There are both invoice.paid and invoice.payment_succeeded but docs lean on the first
 # https://stripe.com/docs/billing/subscriptions/webhooks#tracking
-@webhooks.handler("invoice.paid")
-def subscriptions_invoice_paid(event, **kwargs):
-    print("*** invoice paid! ***")
-    # djstripe.models.Subscription.get
+@webhooks.handler("charge.succeeded")
+def subscriptions_invoice_paid(event):
+    merge_data = {
+        "FIRST_NAME": event.customer.name or "Maker",
+    }
+
+    text_body = render_to_string("email/subscriptions_payment_succeeded_body.txt", merge_data)
+    # html_body = render_to_string("email/subscriptions_subscription_expiring_body.html", merge_data)
+    subject = render_to_string("email/subscriptions_payment_succeeded_subject.txt", merge_data)
+
+    message = AnymailMessage(
+        subject=subject,
+        body=text_body,
+        to=[event.customer.email],
+    )
+    # message.attach_alternative(html_body, "text/html")
+    message.send()
 
 
 class SubscriptionsConfig(AppConfig):
