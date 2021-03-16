282030fac53b3d6e333571a8fb382f417887aba4
Dimitris Zervas <dzervas@dzervas.gr>
2021-03-17 01:49:56 +0200

    Add user trialing

--- print_nanny_webapp/subscriptions/models.py
+++ print_nanny_webapp/subscriptions/models.py
@@ -1,4 +1,21 @@
+from time import timezone
+from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
-from django.db import models
+from django.dispatch import receiver
+
+import djstripe.models
+import logging
+
+logger = logging.getLogger(__name__)
 
 User = get_user_model()
+
+@receiver(post_save, sender=User)
+def start_trial(sender, instance=None, **kwargs):
+    if not instance._state.adding:
+        return
+
+    customer = djstripe.models.Customer.create(subscriber=instance)
+
+    plan = djstripe.models.Plan.objects.first()
+    customer.subscribe(plan, charge_immediately=False, trial_period_days=14)
