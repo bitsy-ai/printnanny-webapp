d1511cf10d8e3dc4892733786b825af656c4556b
Dimitris Zervas <dzervas@dzervas.gr>
2021-03-20 14:52:23 +0200

    Add trialing/premium status

--- print_nanny_webapp/subscriptions/models.py
+++ print_nanny_webapp/subscriptions/models.py
@@ -1,14 +1,15 @@
-from time import timezone
+from django.utils import timezone
 from django.db.models.signals import post_save
-from django.contrib.auth import get_user_model
+from django.contrib import auth
 from django.dispatch import receiver
+from djstripe.enums import SubscriptionStatus
 
 import djstripe.models
 import logging
 
 logger = logging.getLogger(__name__)
 
-User = get_user_model()
+User = auth.get_user_model()
 
 @receiver(post_save, sender=User)
 def start_trial(sender, instance=None, **kwargs):
@@ -19,3 +20,23 @@ def start_trial(sender, instance=None, **kwargs):
 
     plan = djstripe.models.Plan.objects.first()
     customer.subscribe(plan, charge_immediately=False, trial_period_days=14)
+
+def is_trialing(self) -> bool:
+    # Even if one subscription is active normally, the user is not considered
+    # to be trialing
+    if self.is_subscribed():
+        return False
+
+    customer = djstripe.models.Customer.objects.get(subscriber=self)
+    subscriptions = customer.subscriptions.filter(
+        current_period_end__gt=timezone.now(),
+        status=SubscriptionStatus.trialing,
+    )
+
+    return len(subscriptions) > 0
+User.add_to_class('is_trialing', is_trialing)
+
+def is_subscribed(self) -> bool:
+    customer = djstripe.models.Customer.objects.get(subscriber=self)
+    return customer.has_any_active_subscription()
+User.add_to_class('is_subscribed', is_subscribed)
