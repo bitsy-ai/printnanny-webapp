8b10614dbb349241609a20f774a12ace9482c149
Leigh Johnson <hi@leighjohnson.me>
2021-04-26 12:46:09 -0700

    make octoprint first run tests resilient to intermediary states

--- cypress/integration/setup/000-octoprint-first-run.js
+++ cypress/integration/setup/000-octoprint-first-run.js
@@ -3,8 +3,10 @@ import { onlyOn, skipOn } from '@cypress/skip-test'
 describe('OctoPrint initial setup', () => {
   const PRINT_NANNY_EMAIL = Cypress.env('PRINT_NANNY_EMAIL')
   const PRINT_NANNY_PASSWORD = Cypress.env('PRINT_NANNY_PASSWORD')
+  const PRINT_NANNY_TOKEN = Cypress.env('PRINT_NANNY_TOKEN')
   before(() => {
     cy.visit(Cypress.env('OCTOPRINT_URL'), {timeout: 600000}) // sandbox: wait for octoprint deployment to be available
+
   })
 
   it('Complete setup wizard or pass if already completed', () => {
@@ -35,6 +37,8 @@ describe('OctoPrint initial setup', () => {
       cy.get('button[name=next]').click()
       cy.get('button[name=next]').click()
       cy.get('button[name=finish]').click()
+    } else {
+      console.log("Skipping OctoPrint initial setup wizard (already done)")
     }
 
   })
@@ -48,23 +52,32 @@ describe('OctoPrint initial setup', () => {
 
     cy.get('#settings_plugin_pluginmanager_repositorydialog form .btn.btn-primary').first().click({force: true})
     cy.contains('A restart is needed for the changes to take effect.', {timeout: 60000})
-    
-    cy.contains('Restart now', {timeout: 60000}).click()
-        .get('.modal-footer .btn').contains('Proceed').click()
-        .get('# reloadui_overlay button').contains('Reload now').click()
-        .get('#tab_plugin_octoprint_nanny')
-    })
 
+    cy.contains('Restart now', {timeout: 60000}).click().then(() =>{
+      
+      const update_available = Cypress.$('.ui-pnotify[aria-live=assertive] h4').text() == 'Update Available'
+      if (update_available){
+        console.log('Dismissing notification')
+        cy.get('.ui-pnotify[aria-live=assertive]').find('button').contains('Ignore').click()
+  
+      }
+      return cy.get('.modal-footer .btn').contains('Proceed').click()
+        .get('#reloadui_overlay button').contains('Reload now').click({timeout: 60000})
+    })
+        
+
+  })
+  
   it('Runs Print Nanny setup wizard (OctoPrint)', () => {
-      if (Cypress.$('#plugin_octoprint_nanny_wizard').length > 0){
-          cy.octoprintLogin(PRINT_NANNY_EMAIL, PRINT_NANNY_PASSWORD)
+      cy.octoprintLogin(PRINT_NANNY_EMAIL, PRINT_NANNY_PASSWORD).then( ()=>{
+        if (Cypress.$('#octoprint_nanny_wizard_basic').length > 0){
           cy.get('#octoprint_nanny_wizard_basic #octoprint_nanny_settings_auth_token_basic').clear().type(PRINT_NANNY_TOKEN)
           cy.get('#octoprint_nanny_wizard_basic #octoprint_nanny_test_auth_token').click()
               .get('#octoprint_nanny_alert_auth').should('have.class', 'alert-success')
           cy.get('#octoprint_nanny_wizard_basic #octoprint_nanny_device_registration').click()
           cy.get('#octoprint_nanny_wizard_basic #octoprint_nanny_alert_device_registration').should('have.class', 'alert-success')
           cy.get('button[name=finish]').click()
-          cy.get('#tab_plugin_octoprint_nanny').click()
-      }
+        }
+      })
   })
 })
