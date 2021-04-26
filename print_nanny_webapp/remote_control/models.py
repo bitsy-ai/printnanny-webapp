25204cd0796922e78c086ce687ca66d6e7673909
Leigh Johnson <hi@leighjohnson.me>
2021-04-25 22:39:46 -0700

    make hardware, revision, cpu_flags fields nullable (x86 portability)

--- print_nanny_webapp/remote_control/models.py
+++ print_nanny_webapp/remote_control/models.py
@@ -151,10 +151,10 @@ class OctoPrintDevice(SafeDeleteModel):
 
     model = models.CharField(max_length=255)
     platform = models.CharField(max_length=255)
-    cpu_flags = ArrayField(models.CharField(max_length=255))
+    cpu_flags = ArrayField(models.CharField(max_length=255), null=True)
 
-    hardware = models.CharField(max_length=255)  # /cat/cpuinfo HARDWARE
-    revision = models.CharField(max_length=255)  # /cat/cpuinfo REVISION
+    hardware = models.CharField(max_length=255, null=True)  # /cat/cpuinfo HARDWARE
+    revision = models.CharField(max_length=255, null=True)  # /cat/cpuinfo REVISION
     serial = models.CharField(max_length=255)  # /cat/cpuinfo Serial
     cores = models.IntegerField()
     ram = models.IntegerField()
