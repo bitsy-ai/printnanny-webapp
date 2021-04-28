2891523a8ed604e04ff080a14d5c9391c978e6cb
Leigh Johnson <hi@leighjohnson.me>
2021-04-28 16:26:58 -0700

    Bump version: 0.6.0-dev1 → v0.6.0-dev2

--- print_nanny_webapp/__init__.py
+++ print_nanny_webapp/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "__version__ = "__version__ = "0.6.0-dev1"""
+__version__ = "__version__ = "__version__ = "0.6.0-dev2"""
 __version_info__ = tuple(
     [
         int(num) if num.isdigit() else num
