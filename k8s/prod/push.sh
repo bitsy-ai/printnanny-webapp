56127958aa75fac82d3f62ac452a73f008b2329e
Leigh Johnson <hi@leighjohnson.me>
2021-04-28 00:17:00 -0700

    fix prod deploy script

--- k8s/prod/push.sh
+++ k8s/prod/push.sh
@@ -13,10 +13,10 @@ docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django:latest
 docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD)
 
 
-kubectl apply -f k8s/configmap.yml
-kubectl apply -f k8s/octoprint-events.yml
-kubectl apply -f k8s/celery-worker.yml
-kubectl apply -f k8s/django.yml
+kubectl apply -f k8s/prod/configmap.yml
+kubectl apply -f k8s/prod/octoprint-events.yml
+kubectl apply -f k8s/prod/celery-worker.yml
+kubectl apply -f k8s/prod/django.yml
 
 kubectl set image deployment/django django=us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD) --record
 kubectl set image deployment/celery-worker celery-worker=us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD) --record
