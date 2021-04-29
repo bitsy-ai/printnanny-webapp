a0a3187934707a84894baffedcdca1e2201f7fde
Leigh Johnson <hi@leighjohnson.me>
2021-04-28 21:40:25 -0700

    check in codegen

--- clients/python/print_nanny_client/flatbuffers/monitoring/MonitoringEvent.py
+++ clients/python/print_nanny_client/flatbuffers/monitoring/MonitoringEvent.py
@@ -21,19 +21,24 @@ class MonitoringEvent(object):
         self._tab = flatbuffers.table.Table(buf, pos)
 
     # MonitoringEvent
-    def EventDataType(self):
+    def Image(self):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(4))
         if o != 0:
-            return self._tab.Get(flatbuffers.number_types.Uint8Flags, o + self._tab.Pos)
-        return 0
+            x = self._tab.Indirect(o + self._tab.Pos)
+            from print_nanny_client.flatbuffers.monitoring.Image import Image
+            obj = Image()
+            obj.Init(self._tab.Bytes, x)
+            return obj
+        return None
 
     # MonitoringEvent
-    def EventData(self):
+    def BoundingBoxes(self):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
         if o != 0:
-            from flatbuffers.table import Table
-            obj = Table(bytearray(), 0)
-            self._tab.Union(obj, o)
+            x = self._tab.Indirect(o + self._tab.Pos)
+            from print_nanny_client.flatbuffers.monitoring.BoundingBoxes import BoundingBoxes
+            obj = BoundingBoxes()
+            obj.Init(self._tab.Bytes, x)
             return obj
         return None
 
@@ -56,17 +61,17 @@ class MonitoringEvent(object):
         return None
 
 def MonitoringEventStart(builder): builder.StartObject(4)
-def MonitoringEventAddEventDataType(builder, eventDataType): builder.PrependUint8Slot(0, eventDataType, 0)
-def MonitoringEventAddEventData(builder, eventData): builder.PrependUOffsetTRelativeSlot(1, flatbuffers.number_types.UOffsetTFlags.py_type(eventData), 0)
+def MonitoringEventAddImage(builder, image): builder.PrependUOffsetTRelativeSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(image), 0)
+def MonitoringEventAddBoundingBoxes(builder, boundingBoxes): builder.PrependUOffsetTRelativeSlot(1, flatbuffers.number_types.UOffsetTFlags.py_type(boundingBoxes), 0)
 def MonitoringEventAddEventType(builder, eventType): builder.PrependUint8Slot(2, eventType, 0)
 def MonitoringEventAddMetadata(builder, metadata): builder.PrependUOffsetTRelativeSlot(3, flatbuffers.number_types.UOffsetTFlags.py_type(metadata), 0)
 def MonitoringEventEnd(builder): return builder.EndObject()
 
-import print_nanny_client.flatbuffers.monitoring.EventData
+import print_nanny_client.flatbuffers.monitoring.BoundingBoxes
+import print_nanny_client.flatbuffers.monitoring.Image
 import print_nanny_client.flatbuffers.monitoring.Metadata
-import print_nanny_client.flatbuffers.monitoring.MonitoringFrame
 try:
-    from typing import Optional, Union
+    from typing import Optional
 except:
     pass
 
@@ -74,8 +79,8 @@ class MonitoringEventT(object):
 
     # MonitoringEventT
     def __init__(self):
-        self.eventDataType = 0  # type: int
-        self.eventData = None  # type: Union[None, print_nanny_client.flatbuffers.monitoring.MonitoringFrame.MonitoringFrameT]
+        self.image = None  # type: Optional[print_nanny_client.flatbuffers.monitoring.Image.ImageT]
+        self.boundingBoxes = None  # type: Optional[print_nanny_client.flatbuffers.monitoring.BoundingBoxes.BoundingBoxesT]
         self.eventType = 0  # type: int
         self.metadata = None  # type: Optional[print_nanny_client.flatbuffers.monitoring.Metadata.MetadataT]
 
@@ -95,22 +100,27 @@ class MonitoringEventT(object):
     def _UnPack(self, monitoringEvent):
         if monitoringEvent is None:
             return
-        self.eventDataType = monitoringEvent.EventDataType()
-        self.eventData = print_nanny_client.flatbuffers.monitoring.EventData.EventDataCreator(self.eventDataType, monitoringEvent.EventData())
+        if monitoringEvent.Image() is not None:
+            self.image = print_nanny_client.flatbuffers.monitoring.Image.ImageT.InitFromObj(monitoringEvent.Image())
+        if monitoringEvent.BoundingBoxes() is not None:
+            self.boundingBoxes = print_nanny_client.flatbuffers.monitoring.BoundingBoxes.BoundingBoxesT.InitFromObj(monitoringEvent.BoundingBoxes())
         self.eventType = monitoringEvent.EventType()
         if monitoringEvent.Metadata() is not None:
             self.metadata = print_nanny_client.flatbuffers.monitoring.Metadata.MetadataT.InitFromObj(monitoringEvent.Metadata())
 
     # MonitoringEventT
     def Pack(self, builder):
-        if self.eventData is not None:
-            eventData = self.eventData.Pack(builder)
+        if self.image is not None:
+            image = self.image.Pack(builder)
+        if self.boundingBoxes is not None:
+            boundingBoxes = self.boundingBoxes.Pack(builder)
         if self.metadata is not None:
             metadata = self.metadata.Pack(builder)
         MonitoringEventStart(builder)
-        MonitoringEventAddEventDataType(builder, self.eventDataType)
-        if self.eventData is not None:
-            MonitoringEventAddEventData(builder, eventData)
+        if self.image is not None:
+            MonitoringEventAddImage(builder, image)
+        if self.boundingBoxes is not None:
+            MonitoringEventAddBoundingBoxes(builder, boundingBoxes)
         MonitoringEventAddEventType(builder, self.eventType)
         if self.metadata is not None:
             MonitoringEventAddMetadata(builder, metadata)
