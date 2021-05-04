c3bf2c4ecff6ab50ce8020977c400a9b49db9d17
Leigh Johnson <hi@leighjohnson.me>
2021-05-04 01:10:27 -0700

    update image tagging script

--- k8s/prod/push.sh
+++ k8s/prod/push.sh
@@ -3,14 +3,14 @@
 ##
 # app base image
 #
-docker tag print_nanny_webapp_production_django:latest \
-    us.gcr.io/print-nanny/print_nanny_webapp_production_django:latest
+docker tag print_nanny_webapp:$(GIT_SHA) \
+    us.gcr.io/print-nanny/print_nanny_webapp:latest
 
-docker tag print_nanny_webapp_production_django:latest \
-    us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD)
+docker tag print_nanny_webapp:$(GIT_SHA) \
+    us.gcr.io/print-nanny/print_nanny_webapp:$(git rev-parse HEAD)
 
-docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django:latest
-docker push us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD)
+docker push us.gcr.io/print-nanny/print_nanny_webapp:latest
+docker push us.gcr.io/print-nanny/print_nanny_webapp:$(git rev-parse HEAD)
 
 
 kubectl apply -f k8s/prod/configmap.yml
@@ -18,6 +18,6 @@ kubectl apply -f k8s/prod/octoprint-events.yml
 kubectl apply -f k8s/prod/celery-worker.yml
 kubectl apply -f k8s/prod/django.yml
 
-kubectl set image deployment/django django=us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD) --record
-kubectl set image deployment/celery-worker celery-worker=us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD) --record
-kubectl set image deployment/octoprint-events octoprint-events=us.gcr.io/print-nanny/print_nanny_webapp_production_django:$(git rev-parse HEAD) --record
+kubectl set image deployment/django django=us.gcr.io/print-nanny/print_nanny_webapp:$(git rev-parse HEAD) --record
+kubectl set image deployment/celery-worker celery-worker=us.gcr.io/print-nanny/print_nanny_webapp:$(git rev-parse HEAD) --record
+kubectl set image deployment/octoprint-events octoprint-events=us.gcr.io/print-nanny/print_nanny_webapp:$(git rev-parse HEAD) --record
