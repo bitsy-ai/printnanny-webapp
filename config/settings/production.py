d68f8f189c8ca08428d2d014d0bb037f3e98432c
Leigh Johnson <hi@leighjohnson.me>
2021-05-07 14:08:08 -0700

    fix production CORS

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,12 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["print-nanny.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[
+    "print-nanny.com",
+    "www.print-nanny.com",
+    # TODO     
+    # "api.print-nanny.com",
+])
 
 # DATABASES
 # ------------------------------------------------------------------------------
