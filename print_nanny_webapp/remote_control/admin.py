e955422aaa05971018ad2913eaf3c2b9ad8b9278
Leigh Johnson <hi@leighjohnson.me>
2021-05-07 20:09:03 -0700

    add OctoPrintDeviceAdmin section

--- print_nanny_webapp/remote_control/admin.py
+++ print_nanny_webapp/remote_control/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.apps import apps
 # Register your models here.
+
+OctoPrintDevice = apps.get_model("remote_control", "OctoPrintDevice")
+@admin.register(OctoPrintDevice)
+
+class OctoPrintDeviceAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "user", "serial", "cloudiot_device_id")
+    list_filters = ("id", "user", "serial", "cloudiot_device_id")
+    
