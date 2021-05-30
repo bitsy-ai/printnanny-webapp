4467158a96af0e93d573d500720aae0577a7d6ab
Leigh Johnson <hi@leighjohnson.me>
2021-05-29 20:54:47 -0700

    check in environment serializer

--- print_nanny_webapp/telemetry/api/serializers.py
+++ print_nanny_webapp/telemetry/api/serializers.py
@@ -5,43 +5,73 @@ from print_nanny_webapp.telemetry.models import (
     OctoPrintEvent,
     PrintStatusEvent,
     PrintNannyPluginEvent,
+    RemoteCommandEvent,
     TelemetryEvent
 )
+from print_nanny_webapp.telemetry.types import TelemetryEventType
 
+class PlatformSerializer(serializers.DictField):
+    id = serializers.StringField()
+
+# 'environment': {'os': {'id': 'linux', 'platform': 'linux', 'bits': 32}, 'python': {'version': '3.7.3', 'pip': '21.1.2', 'virtualenv': '/home/pi/oprint'}, 
+# 'hardware': {'cores': 4, 'freq': 1200.0, 'ram': 917016576}, 'plugins': 
+# {'pi_support': {'model': 'Raspberry Pi 3 Model B Rev 1.2', 'throttle_state': '0x50000', 'octopi_version': '0.17.0'}}}, 'model_version': None}
+class OctoprintEnvironmentSerializer(serializers.DictField):
+    os = serializers.DictField(
+        id=serializers.CharField(),
+        platform=serializers.CharField(),
+        bits=serializers.CharField()
+    )
+    python = serializers.DictField(
+        version=serializers.CharField(),
+        pip=serializers.CharField(),
+        virtualenv=serializers.CharField()
+    )
+    hardware = serializers.DictField(
+        cores=serializers.IntegerField(),
+        freq=serializers.FloatField(),
+        ram=serializers.IntegerField(),
+    )
+    plugins = serializers.DictField(
+        pi_support=serializers.DictField(
+            model = serializers.CharField(),
+            throttle_state = serializers.CharField(),
+            octopi_version = serializers.CharField()
+        )
+    )
 
 class TelemetryEventSerializer(serializers.ModelSerializer):
-
+    print_session = serializers.StringRelatedField()
+    event_type = serializers.ChoiceField(choices=TelemetryEventType.choices)
+    environment = OctoprintEnvironmentSerializer()
     class Meta:
         model = TelemetryEvent
         fields = "__all__"
         read_only_fields = ("user",)
-    
+
+class PrintStatusEventSerializer(serializers.ModelSerializer):
+    print_session = serializers.StringRelatedField()
+  
+    class Meta:
+        model = PrintStatusEvent
+        fields = "__all__"
+        read_only_fields = ("user",)
 
 class OctoPrintEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = OctoPrintEvent
-        fields = [field.name for field in OctoPrintEvent._meta.fields] + ["url"]
-        extra_kwargs = {
-            "url": {"view_name": "api:octoprint-event-detail", "lookup_field": "id"}
-        }
+        fields = "__all__"
         read_only_fields = ("user",)
 
 
 class PrintNannyPluginEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = PrintNannyPluginEvent
-        fields = [field.name for field in PrintNannyPluginEvent._meta.fields] + ["url"]
-        extra_kwargs = {
-            "url": {"view_name": "api:plugin-event-detail", "lookup_field": "id"}
-        }
+        fields = "__all__"
         read_only_fields = ("user",)
 
-
-class PrintStatusEventSerializer(serializers.ModelSerializer):
+class RemoteCommandEventSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PrintStatusEvent
-        fields = [field.name for field in PrintStatusEvent._meta.fields] + ["url"]
-        extra_kwargs = {
-            "url": {"view_name": "api:print-session-event-detail", "lookup_field": "id"}
-        }
+        model = RemoteCommandEvent
+        fields = "__all__"
         read_only_fields = ("user",)
