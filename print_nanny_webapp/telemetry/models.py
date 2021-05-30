74529b3baa4d67444b2731049abf4275a0edaf6b
Leigh Johnson <hi@leighjohnson.me>
2021-05-30 11:33:01 -0700

    rename plugin_version and client_version fields for clarity

--- print_nanny_webapp/telemetry/models.py
+++ print_nanny_webapp/telemetry/models.py
@@ -35,8 +35,8 @@ class TelemetryEvent(PolymorphicModel):
         on_delete=models.CASCADE,
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
-    plugin_version = models.CharField(max_length=60)
-    client_version = models.CharField(max_length=60)
+    print_nanny_plugin_version = models.CharField(max_length=60)
+    print_nanny_client_version = models.CharField(max_length=60)
     octoprint_version = models.CharField(max_length=60)
     metadata = JSONField(null=True)
     octoprint_job = JSONField(null=True)
