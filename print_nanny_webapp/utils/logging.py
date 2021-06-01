f215732f1eee9752ee26dfd322f81e19441a0b18
Leigh Johnson <hi@leighjohnson.me>
2021-05-31 18:32:23 -0700

    fix print_job_status and print_progress missing on device details view

--- print_nanny_webapp/utils/logging.py
+++ print_nanny_webapp/utils/logging.py
@@ -2,9 +2,12 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class ExcludeHealthEndpoint(logging.Filter):
     ENDPOINT = "health"
 
     def filter(self, record: logging.LogRecord) -> bool:
-        logging.info(f"ExcludeHealthEndpoin received record={record} with message {record.getMessage()}")
+        logging.info(
+            f"ExcludeHealthEndpoin received record={record} with message {record.getMessage()}"
+        )
         return self.ENDPOINT not in record.getMessage()
