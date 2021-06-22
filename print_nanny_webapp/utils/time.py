53728475935709c95124e37333fa3e688c72dc1d
Leigh Johnson <hi@leighjohnson.me>
2021-06-21 22:09:04 -0700

    pretty print timedelta in DD:HH:SS format

---
+++ print_nanny_webapp/utils/time.py
@@ -0,0 +1,13 @@
+# https://gist.github.com/thatalextaylor/7408395
+def pretty_time_delta(seconds: int) -> str:
+    days, seconds = divmod(seconds, 86400)
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    if days > 0:
+        return "%dd%dh%dm%ds" % (days, hours, minutes, seconds)
+    elif hours > 0:
+        return "%dh%dm%ds" % (hours, minutes, seconds)
+    elif minutes > 0:
+        return "%dm%ds" % (minutes, seconds)
+    else:
+        return "%ds" % (seconds,)
