cb0159ea90a74d22615fcdf1be0c5011360f56f9
Leigh Johnson <hi@leighjohnson.me>
2021-06-25 12:05:37 -0700

    add test_no_sold_out_redirect

--- print_nanny_webapp/subscriptions/tests/test_views.py
+++ print_nanny_webapp/subscriptions/tests/test_views.py
@@ -10,8 +10,16 @@ from print_nanny_webapp.subscriptions.views import (
 
 
 @pytest.mark.django_db
-def test_get_redirect_url(user: User, client, settings: "django.conf.settings"):
+def test_sold_out_redirect(user: User, client, settings: "django.conf.settings"):
     settings.PAID_BETA_SUBSCRIPTION_LIMIT = 0
     client.force_login(user)
     response = client.get("/subscriptions/founding-member")
     assertRedirects(response, "/subscriptions/sold-out")
+
+
+@pytest.mark.django_db
+def test_no_sold_out_redirect(user: User, client, settings: "django.conf.settings"):
+    settings.PAID_BETA_SUBSCRIPTION_LIMIT = 10
+    client.force_login(user)
+    response = client.get("/subscriptions/founding-member")
+    assert response.status_code == 200
