7b1c33afdd6c5ebe3192b69eaed9fe2dde734a35
Leigh Johnson <hi@leighjohnson.me>
2021-07-17 00:02:07 -0700

    0.8.4-dev11 client codegen ✨

--- clients/python/print_nanny_client/__init__.py
+++ clients/python/print_nanny_client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.8.4-dev10"
+__version__ = "0.8.4-dev11"
 
 # import apis into sdk package
 from print_nanny_client.api.alerts_api import AlertsApi
