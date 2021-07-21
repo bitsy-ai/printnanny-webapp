8f40ce8ad3a38fbb06c63812050cacaf45c6fd69
Leigh Johnson <hi@leighjohnson.me>
2021-07-20 22:45:26 -0700

    0.8.9 client codegen ✨

--- clients/kotlin/src/main/kotlin/com/print-nanny/client/models/OctoPrintDeviceKey.kt
+++ clients/kotlin/src/main/kotlin/com/print-nanny/client/models/OctoPrintDeviceKey.kt
@@ -19,111 +19,96 @@ import java.io.Serializable
 
 /**
  * 
- * @param id 
- * @param deleted 
- * @param createdDt 
- * @param name 
- * @param user 
- * @param publicKey 
- * @param fingerprint 
- * @param cloudiotDevice 
+ * @param caCerts 
+ * @param cloudiotDeviceConfigs 
  * @param cloudiotDeviceName 
- * @param cloudiotDevicePath 
  * @param cloudiotDeviceNumId 
- * @param model 
- * @param platform 
- * @param serial 
+ * @param cloudiotDevicePath 
+ * @param cloudiotDevice 
  * @param cores 
- * @param ram 
- * @param pythonVersion 
- * @param pipVersion 
+ * @param createdDt 
+ * @param manageUrl 
+ * @param model 
+ * @param monitoringActive 
+ * @param name 
  * @param octoprintVersion 
+ * @param pipVersion 
+ * @param platform 
  * @param pluginVersion 
  * @param printNannyClientVersion 
- * @param url 
- * @param privateKey 
  * @param privateKeyChecksum 
+ * @param privateKey 
  * @param publicKeyChecksum 
- * @param cloudiotDeviceConfigs 
- * @param caCerts 
- * @param manageUrl 
- * @param monitoringActive 
+ * @param publicKey 
+ * @param pythonVersion 
+ * @param ram 
+ * @param serial 
+ * @param user 
+ * @param activeSession 
  * @param cpuFlags 
  * @param hardware 
  * @param revision 
- * @param virtualenv 
- * @param activeSession 
  */
 
 data class OctoPrintDeviceKey (
-    @Json(name = "id")
-    val id: kotlin.Int,
-    @Json(name = "deleted")
-    val deleted: java.time.OffsetDateTime,
-    @Json(name = "created_dt")
-    val createdDt: java.time.OffsetDateTime,
-    @Json(name = "name")
-    val name: kotlin.String,
-    @Json(name = "user")
-    val user: kotlin.Int,
-    @Json(name = "public_key")
-    val publicKey: kotlin.String,
-    @Json(name = "fingerprint")
-    val fingerprint: kotlin.String,
-    @Json(name = "cloudiot_device")
-    val cloudiotDevice: kotlin.collections.Map<kotlin.String, AnyType>,
+    @Json(name = "ca_certs")
+    val caCerts: kotlin.collections.Map<kotlin.String, kotlin.String>,
+    @Json(name = "cloudiot_device_configs")
+    val cloudiotDeviceConfigs: kotlin.String,
     @Json(name = "cloudiot_device_name")
     val cloudiotDeviceName: kotlin.String,
-    @Json(name = "cloudiot_device_path")
-    val cloudiotDevicePath: kotlin.String,
     @Json(name = "cloudiot_device_num_id")
     val cloudiotDeviceNumId: kotlin.Int,
-    @Json(name = "model")
-    val model: kotlin.String,
-    @Json(name = "platform")
-    val platform: kotlin.String,
-    @Json(name = "serial")
-    val serial: kotlin.String,
+    @Json(name = "cloudiot_device_path")
+    val cloudiotDevicePath: kotlin.String,
+    @Json(name = "cloudiot_device")
+    val cloudiotDevice: kotlin.collections.Map<kotlin.String, AnyType>,
     @Json(name = "cores")
     val cores: kotlin.Int,
-    @Json(name = "ram")
-    val ram: kotlin.Int,
-    @Json(name = "python_version")
-    val pythonVersion: kotlin.String,
-    @Json(name = "pip_version")
-    val pipVersion: kotlin.String,
+    @Json(name = "created_dt")
+    val createdDt: java.time.OffsetDateTime,
+    @Json(name = "manage_url")
+    val manageUrl: java.net.URI,
+    @Json(name = "model")
+    val model: kotlin.String,
+    @Json(name = "monitoring_active")
+    val monitoringActive: kotlin.Boolean,
+    @Json(name = "name")
+    val name: kotlin.String,
     @Json(name = "octoprint_version")
     val octoprintVersion: kotlin.String,
+    @Json(name = "pip_version")
+    val pipVersion: kotlin.String,
+    @Json(name = "platform")
+    val platform: kotlin.String,
     @Json(name = "plugin_version")
     val pluginVersion: kotlin.String,
     @Json(name = "print_nanny_client_version")
     val printNannyClientVersion: kotlin.String,
-    @Json(name = "url")
-    val url: java.net.URI,
-    @Json(name = "private_key")
-    val privateKey: kotlin.String,
     @Json(name = "private_key_checksum")
     val privateKeyChecksum: kotlin.String,
+    @Json(name = "private_key")
+    val privateKey: kotlin.String,
     @Json(name = "public_key_checksum")
     val publicKeyChecksum: kotlin.String,
-    @Json(name = "cloudiot_device_configs")
-    val cloudiotDeviceConfigs: kotlin.String,
-    @Json(name = "ca_certs")
-    val caCerts: kotlin.collections.Map<kotlin.String, kotlin.String>,
-    @Json(name = "manage_url")
-    val manageUrl: java.net.URI,
-    @Json(name = "monitoring_active")
-    val monitoringActive: kotlin.Boolean,
+    @Json(name = "public_key")
+    val publicKey: kotlin.String,
+    @Json(name = "python_version")
+    val pythonVersion: kotlin.String,
+    @Json(name = "ram")
+    val ram: kotlin.Int,
+    @Json(name = "serial")
+    val serial: kotlin.String,
+    @Json(name = "user")
+    val user: kotlin.Int,
+    @Json(name = "active_session")
+    val activeSession: PrintSession? = null,
     @Json(name = "cpu_flags")
     val cpuFlags: kotlin.collections.List<kotlin.String>? = null,
     @Json(name = "hardware")
     val hardware: kotlin.String? = null,
     @Json(name = "revision")
-    val revision: kotlin.String? = null,
-    @Json(name = "virtualenv")
-    val virtualenv: kotlin.String? = null,
-    @Json(name = "active_session")
-    val activeSession: PrintSession? = null
+    val revision: kotlin.String? = null
 ) : Serializable {
     companion object {
         private const val serialVersionUID: Long = 123
