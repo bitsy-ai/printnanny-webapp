85de1af794d055c53c59b70f3203d8d5f5cf5caa
Leigh Johnson <hi@leighjohnson.me>
2021-07-20 20:51:27 -0700

    0.8.7 client codegen ✨

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_namespace_packages  # noqa: H301
 
 NAME = "print-nanny-client"
-VERSION = "0.8.6-dev3"
+VERSION = "0.8.7"
 # To install the library, run the following
 #
 # python setup.py install
