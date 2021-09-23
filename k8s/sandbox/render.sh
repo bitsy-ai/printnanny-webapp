eebbe2e432cd820f3a964c21ff0541835899d283
Leigh Johnson <hi@leighjohnson.me>
2021-09-22 21:15:45 -0700

    update configmap template

--- k8s/sandbox/render.sh
+++ k8s/sandbox/render.sh
@@ -4,6 +4,7 @@ set -u
 
 pip install j2cli[yaml]
 
+j2 k8s/sandbox/configmap.j2 -o k8s/sandbox/configmap.yml
 j2 k8s/sandbox/monolith.j2 -o k8s/sandbox/monolith.yml
 echo "🌳 Generated deployment config for $PRINT_NANNY_USER"
 cat k8s/sandbox/monolith.yml
