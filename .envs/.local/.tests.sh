401dc24a88f8eb3ee27a48bfcdbe0c6e79c7c180
Leigh Johnson <hi@leighjohnson.me>
2021-09-22 19:42:34 -0700

    add COTURN_DATABASE_URL to tests environment

--- .envs/.local/.tests.sh
+++ .envs/.local/.tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export DATABASE_URL="postgres://debug:debug@localhost:5432/print_nanny"
+export COTURN_SECRET_KEY="testsecret1234"
+export COTURN_REALM="turn.test.com"
+export COTURN_DATABASE_URL="postgres://debug:debug@localhost:5432/coturn"
 export REDIS_URL=redis://localhost:6379/0
 export CELERY_BROKER_URL=redis://localhost:6379/0
 export MAILGUN_API_KEY=""
