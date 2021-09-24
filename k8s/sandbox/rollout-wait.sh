44aa062e119b046b0dd97b260a1eceb41e567188
Leigh Johnson <hi@leighjohnson.me>
2021-09-24 09:54:15 -0700

    rm octoprint rollout-wait from sandbox deploy

--- k8s/sandbox/rollout-wait.sh
+++ k8s/sandbox/rollout-wait.sh
@@ -13,11 +13,11 @@ until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
   sleep $SLEEP
 done
 
-DEPLOYMENT="deployment/octoprint"
-ROLLOUT_STATUS_CMD="kubectl rollout status $DEPLOYMENT"
-until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
-  $ROLLOUT_STATUS_CMD
-  ATTEMPTS=$((attempts + 1))
-  echo "Waiting $SLEEP sec for $DEPLOYMENT rollout to complete"
-  sleep $SLEEP
-done
+# DEPLOYMENT="deployment/octoprint"
+# ROLLOUT_STATUS_CMD="kubectl rollout status $DEPLOYMENT"
+# until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
+#   $ROLLOUT_STATUS_CMD
+#   ATTEMPTS=$((attempts + 1))
+#   echo "Waiting $SLEEP sec for $DEPLOYMENT rollout to complete"
+#   sleep $SLEEP
+# done
