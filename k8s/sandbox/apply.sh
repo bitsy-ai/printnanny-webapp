4ccaf2ca8f59d06a6643e454a157edb3040fbf5b
Leigh Johnson <leigh@bitsy.ai>
2021-09-24 19:27:18 -0700

    add optional purge_data input (defaults to false) on sandbox deploy (#221)

--- k8s/sandbox/apply.sh
+++ k8s/sandbox/apply.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 
 set -eu
-echo "🌻 Creating sandbox environment for $PRINT_NANNY_USER"
+echo "🌻 Creating sandbox environment from $GITHUB_SHA"
 kubectl apply -f k8s/sandbox/configmap.yml
 kubectl apply -f k8s/sandbox/monolith.yml
 kubectl apply -f k8s/sandbox/certs.yml
+kubectl apply -f k8s/sandbox/pv.yml
