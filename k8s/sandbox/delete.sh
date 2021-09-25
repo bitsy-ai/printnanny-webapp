41d9875b16226cea9ee7e27c41c5f3a3685d61ab
Leigh Johnson <hi@leighjohnson.me>
2021-09-24 19:33:02 -0700

    rm old delete script

--- k8s/sandbox/delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -uo pipefail
-
-echo "💥 Tearing down sandbox environment for $PRINT_NANNY_USER"
-kubectl delete --ignore-not-found=true -f k8s/sandbox/configmap.yml
-kubectl delete --ignore-not-found=true -f k8s/sandbox/monolith.yml
