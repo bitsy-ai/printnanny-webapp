f1129a3966921229bcfebba527293a60fdbdf745
Leigh Johnson <leigh@bitsy.ai>
2021-10-16 21:40:17 -0700

    Rust client thiserror (#234)
    * derive thiserror::Error
    
    * Bump version: 0.8.39 → 0.8.40
    
    * 0.8.40 client codegen ✨
    
    * 0.8.40 client codegen ✨
    
    * 0.8.40 client codegen ✨
    
    * 0.8.40 client codegen ✨
    
    * 0.8.40 client codegen ✨
    
    * 0.8.40 client codegen ✨
    
    * 0.8.40 client codegen ✨

--- clients/rust/src/apis/mod.rs
+++ clients/rust/src/apis/mod.rs
@@ -1,7 +1,8 @@
 use std::error;
 use std::fmt;
+use thiserror::{ Error as ThisError };
 
-#[derive(Debug, Clone)]
+#[derive(ThisError, Debug, Clone)]
 pub struct ResponseContent<T> {
     pub status: reqwest::StatusCode,
     pub content: String,
