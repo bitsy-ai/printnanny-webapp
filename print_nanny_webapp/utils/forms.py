6de7414278469610d30fba6407a7d1d8ebfc64dc
Leigh Johnson <hi@leighjohnson.me>
2021-11-13 17:06:34 -0800

    clean up unused forms util

--- print_nanny_webapp/utils/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from crispy_forms.helper import FormHelper
-
-
-class CrispyForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = "post"
