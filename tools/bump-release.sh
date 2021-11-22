c0ea66dcaf677d74bef7a1dce39d7ef12da358e6
Leigh Johnson <leigh@bitsy.ai>
2021-11-21 17:21:38 -0800

    fix bumpversion commit msg

--- tools/bump-release.sh
+++ tools/bump-release.sh
@@ -6,12 +6,12 @@ bump2version --current-version $(cat version.txt) --new-version "$1" patch
 
 make python-client
 git add clients/
-git commit --amend -m "🐍 $1 python openapi client codegen"
+git commit --amend --no-edit
 make python-client-release
 
 make rust-client
 git add clients/
-git commit --amend -m "🦀 $1 typescript openapi client codegen"
+git commit --amend -m --no-edit
 make rust-client-release
 
 git push
