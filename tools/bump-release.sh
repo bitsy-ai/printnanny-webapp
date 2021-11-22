805f47bbbd57a42d16ccc466455cb401e74d20de
Leigh Johnson <leigh@bitsy.ai>
2021-11-21 16:18:13 -0800

    fix typo in bump script, rename script

--- tools/bump-release.sh
+++ tools/bump-release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 bump2version --current-version $(cat version.txt) --new-version "$1" patch
 
 make python-client
@@ -9,7 +11,7 @@ make python-client-release
 
 make rust-client
 git add clients/
-git commit ---amend -m "🦀 $2 typescript openapi client codegen"
+git commit --amend -m "🦀 $2 typescript openapi client codegen"
 make rust-client-release
 
 git push
