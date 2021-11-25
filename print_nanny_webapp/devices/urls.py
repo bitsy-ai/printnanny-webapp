321910cd54e58ca448257e3d144f496e3d72cfb3
Leigh Johnson <leigh@bitsy.ai>
2021-11-24 19:32:19 -0800

    implement device delete + confirmation form

--- print_nanny_webapp/devices/urls.py
+++ print_nanny_webapp/devices/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 from .views import (
     DeviceListView,
+    DeviceDeleteView,
     DeviceDetailView,
     DeviceCreateView,
     DeviceLicenseDownload,
@@ -13,5 +14,6 @@ urlpatterns = [
     path("register", DeviceCreateView.as_view(), name="create"),
     path("", DeviceListView.as_view(), name="list"),
     path("<slug:pk>", DeviceDetailView.as_view(), name="detail"),
+    path("<slug:pk>/delete", DeviceDeleteView.as_view(), name="delete"),
     path("<slug:pk>/license", DeviceLicenseDownload.as_view(), name="license"),
 ]
