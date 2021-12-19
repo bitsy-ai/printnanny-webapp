af9b3a05f57d220a0036ec4672e0ad4194932468
Leigh Johnson <leigh@bitsy.ai>
2021-12-19 15:35:52 -0800

    ensure debug toolbar is always shown by local settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -34,17 +34,23 @@ INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+
+def custom_show_toolbar(request):
+    return True # Always show toolbar
 DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    # "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
+    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER", default=False) == "yes":
-    import socket
-
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+INTERNAL_IPS = [
+    "127.0.0.1", 
+    "10.0.2.2"
+]
+# if env("USE_DOCKER", default=False) == "yes":
+import socket
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
 # django-extensions
 # ------------------------------------------------------------------------------
@@ -115,3 +121,4 @@ LOGGING = {
     }
 
 }
+
