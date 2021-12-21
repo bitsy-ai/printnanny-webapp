766c7e86b254a9b889ea9752869231baec98b3f4
Leigh Johnson <leigh@bitsy.ai>
2021-12-20 20:58:00 -0800

    fix local settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -47,10 +47,10 @@ INTERNAL_IPS = [
     "127.0.0.1", 
     "10.0.2.2"
 ]
-# if env("USE_DOCKER", default=False) == "yes":
-import socket
-hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+if env("USE_DOCKER", default=False) == "yes":
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
 # django-extensions
 # ------------------------------------------------------------------------------
