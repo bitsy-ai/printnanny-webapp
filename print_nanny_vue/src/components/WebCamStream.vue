3b1cf007dc3aead970326a7a9e9d224ff35121f3
Leigh Johnson <leigh@bitsy.ai>
2021-12-24 13:42:15 -0800

    size alert height boxes with align-items-stretch

--- print_nanny_vue/src/components/WebCamStream.vue
+++ print_nanny_vue/src/components/WebCamStream.vue
@@ -44,7 +44,7 @@ export default {
 <div class="card col-12">
   <div class="card-header">
     <h2 class="header-title">
-      <a class="badge badge-light" href="" target="_blank">
+      <a class="badge badge-light" :href="deviceUrl" target="_blank">
         <i class="mdi mdi-printer-3d"></i> {{ devices[deviceId].hostname }}
 
         </a>
@@ -52,19 +52,27 @@ export default {
     </h2>
   </div>
     <div class="card-body">
-        <div class="col-6 offset-3 mt-2">
-        <img src="/static/images/sleep.svg" style="opacity: 30%" class="d-none d-md-block img-fluid"/>
+        <div class="alert alert-info col-12 d-flex" role="alert">
+          <div class="row">
+            <div class="col-8">
+              <h4 class="alert-heading"><i class="mdi mdi-warning"></i>Camera is offline</h4>
+              <p>Click/tap <i class="mdi mdi-camera"></i><strong>Start Monitoring</strong> button to start.</p>
+            </div>
+            <div class="col-4">
+            <img src="/static/images/sleep.svg" style="opacity: 30%" class="d-none d-md-block img-fluid"/>
+            </div>
+          </div>
         </div>
     </div>
     <div class="card-footer d-flex justify-content-center">
-        <a href="#" target="_blank">
-            <button class="btn btn-light btn-sm mr-2 ml-2">
-            <i class="mdi mdi-camera"></i> Start Monitoring
-            </button>
-        </a>
         <a :href="deviceUrl" target="_blank">
             <button class="btn btn-light btn-sm mr-2 ml-2">
                 <i class="mdi mdi-printer-3d"></i> Manage Pi
+            </button>
+        </a>
+        <a href="#" target="_blank">
+            <button class="btn btn-light btn-sm mr-2 ml-2">
+            <i class="mdi mdi-camera"></i> Start Monitoring
             </button>
         </a>
     </div>
