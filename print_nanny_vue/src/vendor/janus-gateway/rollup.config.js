fef94a887b160cc2e4761efb39aa3151f9ef2237
Leigh Johnson <leigh@bitsy.ai>
2021-12-24 22:01:21 -0800

    rename includes in janus-gateway

--- print_nanny_vue/src/vendor/janus-gateway/rollup.config.js
+++ print_nanny_vue/src/vendor/janus-gateway/rollup.config.js
@@ -3,17 +3,16 @@ import * as fs from 'fs';
 
 export default {
     name: 'Janus',
-    input: 'module.js',
+    input: '_module.js',
     output: {
         strict: false,
         format: 'es',
-        file: 'bundle.js'
+        file: 'janus.js'
     },
     plugins: [
         replace({
-            JANUS_CODE: fs.readFileSync('janus.js', 'utf-8'),
+            JANUS_CODE: fs.readFileSync('lib.js', 'utf-8'),
             delimiters: ['@','@'],
-            includes: 'module.js'
         })
     ]
 };
