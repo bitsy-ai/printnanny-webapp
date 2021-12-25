c02d8ecff2f2f4d9682412afb5c706ca0c7b9c4b
Leigh Johnson <leigh@bitsy.ai>
2021-12-24 22:02:47 -0800

    fix missing import

--- print_nanny_vue/src/vendor/janus-gateway/janus.js
+++ print_nanny_vue/src/vendor/janus-gateway/janus.js
@@ -31,6 +31,7 @@
 	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 	OTHER DEALINGS IN THE SOFTWARE.
  */
+import adapter from 'webrtc-adapter';
 
 // List of sessions
 Janus.sessions = {};
