c7e96c8e2b67ceccac760d11321002d583498d05
Leigh Johnson <leigh@bitsy.ai>
2022-01-02 23:15:07 -0800

    set ARA_SETTINGS and use django-allowed-cidr

--- compose/production/ara/settings.py
+++ compose/production/ara/settings.py
@@ -1,1 +1,27 @@
-USE_X_FORWARDED_HOST = True
+# from django.conf import settings
+# EXTERNAL_AUTH = settings.get("EXTERNAL_AUTH", False, "@bool")
+# if EXTERNAL_AUTH:
+#     EXTERNAL_AUTH_MIDDLEWARE = ["django.contrib.auth.middleware.RemoteUserMiddleware"]
+#     AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.RemoteUserBackend"]
+#     REST_FRAMEWORK_AUTH = ("rest_framework.authentication.RemoteUserAuthentication",)
+# else:
+REST_FRAMEWORK_AUTH = ("rest_framework.authentication.BasicAuthentication",)
+
+# fmt: off
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+] + [
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware"
+]
+MIDDLEWARE += [ 'allow_cidr.middleware.AllowCIDRMiddleware']
+ALLOWED_CIDR_NETS = [
+    '10.12.0.0/14',
+    '10.16.0.0/20'
+]
