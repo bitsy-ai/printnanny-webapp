3b98e6f43e0f7acd0a516dfa8f4d05d04a2456a2
Leigh Johnson <leigh@bitsy.ai>
2022-01-09 16:09:03 -0800

    update dict merges to use py9 dict | dict syntax

--- print_nanny_webapp/utils/api/views.py
+++ print_nanny_webapp/utils/api/views.py
@@ -13,17 +13,18 @@ generic_list_errors = {
     500: ErrorDetailSerializer,
 }
 
-generic_get_errors = {404: ErrorDetailSerializer}.update(generic_list_errors)
+generic_get_errors = {404: ErrorDetailSerializer} | generic_list_errors
 
 
-generic_create_errors = {409: ErrorDetailSerializer}.update(generic_list_errors)
+generic_create_errors = {409: ErrorDetailSerializer} | generic_list_errors
 
 generic_update_errors = generic_create_errors
 
 list_desired_config_schema = extend_schema(
     responses={
         200: PrintNannyApiConfigSerializer,
-    }.update(generic_list_errors),
+    }
+    | generic_list_errors,
 )
 
 
