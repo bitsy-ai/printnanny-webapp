65bd65371417c78ba3fbd2a3648de347465db9bb
Leigh Johnson <leigh@bitsy.ai>
2022-01-16 12:51:08 -0800

    shellcheck k8s/prod/*.sh

--- k8s/prod/push.sh
+++ k8s/prod/push.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 ##
 # app base image
 #
-docker tag print_nanny_webapp:${GIT_SHA} \
+docker tag "print_nanny_webapp:${GIT_SHA}" \
     us.gcr.io/print-nanny/print_nanny_webapp:latest
 
-docker tag print_nanny_webapp:${GIT_SHA} \
-    us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA}
+docker tag "print_nanny_webapp:${GIT_SHA}" \
+    "us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA}"
 
 docker push us.gcr.io/print-nanny/print_nanny_webapp:latest
-docker push us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA}
+docker push "us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA}"
 
 
 kubectl apply -f k8s/stable/configmap.yml
@@ -21,8 +21,8 @@ kubectl apply -f k8s/prod/octoprint-events.yml
 kubectl apply -f k8s/prod/celery-worker.yml
 kubectl apply -f k8s/prod/django.yml
 
-kubectl set image deployment/ara ara=us.gcr.io/print-nanny/ara:${GIT_SHA} --record
-kubectl set image deployment/django django=us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA} --record
+kubectl set image "deployment/ara ara=us.gcr.io/print-nanny/ara:${GIT_SHA}" --record
+kubectl set image "deployment/django django=us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA}" --record
 ATTEMPTS=0
 MAX_ATTEMPTS=30
 SLEEP=10
@@ -30,11 +30,11 @@ DEPLOYMENT="deployment/django"
 ROLLOUT_STATUS_CMD="kubectl rollout status $DEPLOYMENT"
 until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
   $ROLLOUT_STATUS_CMD
-  ATTEMPTS=$((attempts + 1))
+  ATTEMPTS=$((ATTEMPTS + 1))
   echo "Waiting $SLEEP sec for $DEPLOYMENT rollout to complete"
   sleep $SLEEP
 done
 
-kubectl set image deployment/celery-worker celery-worker=us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA} --record
-kubectl set image deployment/octoprint-events octoprint-events=us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA} --record
-kubectl set image deployment/octoprint-events alerts=us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA} --record
+kubectl set image "deployment/celery-worker celery-worker=us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA}" --record
+kubectl set image "deployment/octoprint-events octoprint-events=us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA}" --record
+kubectl set image "deployment/octoprint-events alerts=us.gcr.io/print-nanny/print_nanny_webapp:${GIT_SHA}" --record
