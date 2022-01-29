6834d67cc29ceff2d212fe93e7046618d9ffa3c6
Leigh Johnson <leigh@bitsy.ai>
2022-01-28 18:14:56 -0800

    connect post_save signal on DeviceEvent

--- print_nanny_webapp/events/signals.py
+++ print_nanny_webapp/events/signals.py
@@ -1,10 +1,15 @@
 import logging
 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from print_nanny_webapp.events.models import DeviceEvent
-from .services import publish_cloudiot_event
+from .services import publish_cloudiot_command
 
 logger = logging.getLogger(__name__)
 
 
+@receiver(post_save, sender=DeviceEvent, dispatch_uid="create_device_link_task")
 def publish_device_event(sender, instance: DeviceEvent, created, **kwargs):
-    return publish_cloudiot_event(instance)
+    if instance.command:
+        logger.info(f"Attempting to publish DeviceEvent to command topic {instance}")
+        publish_cloudiot_command(instance)
