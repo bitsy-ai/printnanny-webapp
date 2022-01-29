4fdc47ec49afd221dcd6b13a1ffbf208d1efc2fe
Leigh Johnson <leigh@bitsy.ai>
2022-01-28 21:48:29 -0800

    Bump version: 0.46.3 → 0.47.0

--- clients/python/printnanny_api_client/__init__.py
+++ clients/python/printnanny_api_client/__init__.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.46.3"
+__version__ = "0.47.0"
 
 # import apis into sdk package
 from printnanny_api_client.api.alerts_api import AlertsApi
