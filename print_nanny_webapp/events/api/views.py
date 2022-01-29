3a5c702593688f9dd4786275efffd466184c2269
Leigh Johnson <leigh@bitsy.ai>
2022-01-28 20:56:45 -0800

    fix event payload serialization, implement PUT/PATCH methods on events eventpoint

--- print_nanny_webapp/events/api/views.py
+++ print_nanny_webapp/events/api/views.py
@@ -4,36 +4,29 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from django.apps import apps
-from drf_spectacular.utils import PolymorphicProxySerializer
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.utils import extend_schema
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin
-from print_nanny_webapp.events.models import Event, TestEvent, DeviceEvent
-from .serializers import PolymorphicEventSerializer, TestEventSerializer
-from print_nanny_webapp.utils.api.views import (
-    generic_create_errors,
-    generic_list_errors,
-    generic_get_errors,
-    generic_update_errors,
+from rest_framework.mixins import (
+    ListModelMixin,
+    RetrieveModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
 )
+from print_nanny_webapp.events.models import DeviceEvent
+from .serializers import PolymorphicEventSerializer
 
 Device = apps.get_model("devices", "Device")
 
 logger = logging.getLogger(__name__)
 
-# @extend_schema_view(
-#     create=extend_schema(
-#         request=PolymorphicEventSerializer,
-#         responses={
-#             201: PolymorphicEventSerializer,
-#         } | generic_create_errors
-#     )
-# )
-
 
 @extend_schema(tags=["events", "devices"])
 class DeviceEventViewSet(
-    GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin
+    GenericViewSet,
+    ListModelMixin,
+    RetrieveModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
 ):
     serializer_class = PolymorphicEventSerializer
     queryset = DeviceEvent.objects.all()
