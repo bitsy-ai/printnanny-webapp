72115ad1af35f267ba54949995e1028e3d0c716a
Release Automation <leigh@bitsy.ai>
2022-02-18 12:51:08 -0800

    rename front-end Vuex store tasks -> events

---
+++ print_nanny_vue/src/store/events/state.js
@@ -0,0 +1,6 @@
+export const SENT_EVENTS = 'sent_events'
+export const RECEIVED_EVENTS = 'received_events'
+export default {
+  [SENT_EVENTS]: [],
+  [RECEIVED_EVENTS]: []
+}
--- print_nanny_vue/src/store/tasks/state.js
+++
@@ -1,6 +0,0 @@
-export const ACTIVE_TASKS = 'active_tasks'
-export const ACTIVE_MONITORING_TASK = 'active_monitoring_task'
-export default {
-  [ACTIVE_TASKS]: [],
-  [ACTIVE_MONITORING_TASK]: {}
-}
