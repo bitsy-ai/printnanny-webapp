decd58c54b990a18216a94b9173a384bc7aaccbc
Release Automation <leigh@bitsy.ai>
2022-02-19 13:58:30 -0800

    cp RustClientCodegen.java RustClientCodegenTest.java from 0eca6291275394dab1bff79c9b4b6737202a9be9

--- client-templates/rust-client/src/test/java/com/printnanny/api/rust/RustClientGeneratorTest.java
+++ client-templates/rust-client/src/test/java/com/printnanny/api/rust/RustClientGeneratorTest.java
@@ -1,36 +1,56 @@
 package com.printnanny.api.rust;
 
-import org.junit.Test;
-import org.openapitools.codegen.ClientOptInput;
-import org.openapitools.codegen.DefaultGenerator;
-import org.openapitools.codegen.config.CodegenConfigurator;
-
-/***
- * This test allows you to easily launch your code generation software under a debugger.
- * Then run this test under debug mode.  You will be able to step through your java code
- * and then see the results in the out directory.
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ * Copyright 2018 SmartBear Software
  *
- * To experiment with debugging your code generator:
- * 1) Set a break point in RustClientGenerator.java in the postProcessOperationsWithModels() method.
- * 2) To launch this test in Eclipse: right-click | Debug As | JUnit Test
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-public class RustClientGeneratorTest {
 
-  // use this test to launch you code generator in the debugger.
-  // this allows you to easily set break points in MyclientcodegenGenerator.
+import org.openapitools.codegen.CodegenConstants;
+import org.openapitools.codegen.languages.RustClientCodegen;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RustClientCodegenTest {
+
   @Test
-  public void launchCodeGenerator() {
-    // to understand how the 'openapi-generator-cli' module is using 'CodegenConfigurator', have a look at the 'Generate' class:
-    // https://github.com/OpenAPITools/openapi-generator/blob/master/modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/Generate.java
-    final CodegenConfigurator configurator = new CodegenConfigurator()
-              .setGeneratorName("rust-client") // use this codegen library
-              .setInputSpec("../../../modules/openapi-generator/src/test/resources/2_0/petstore.yaml") // sample OpenAPI file
-              // .setInputSpec("https://raw.githubusercontent.com/openapitools/openapi-generator/master/modules/openapi-generator/src/test/resources/2_0/petstore.yaml") // or from the server
-              .setOutputDir("out/rust-client"); // output directory
+  public void testInitialConfigValues() throws Exception {
+    final RustClientCodegen codegen = new RustClientCodegen();
+    codegen.processOpts();
 
-    final ClientOptInput clientOptInput = configurator.toClientOptInput();
-    DefaultGenerator generator = new DefaultGenerator();
-    generator.opts(clientOptInput).generate();
+    Assert.assertEquals(codegen.additionalProperties().get(CodegenConstants.HIDE_GENERATION_TIMESTAMP), Boolean.TRUE);
+    Assert.assertEquals(codegen.isHideGenerationTimestamp(), true);
   }
+
+  @Test
+  public void testSettersForConfigValues() throws Exception {
+    final RustClientCodegen codegen = new RustClientCodegen();
+    codegen.setHideGenerationTimestamp(false);
+    codegen.processOpts();
+
+    Assert.assertEquals(codegen.additionalProperties().get(CodegenConstants.HIDE_GENERATION_TIMESTAMP), Boolean.FALSE);
+    Assert.assertEquals(codegen.isHideGenerationTimestamp(), false);
+  }
+
+  @Test
+  public void testAdditionalPropertiesPutForConfigValues() throws Exception {
+    final RustClientCodegen codegen = new RustClientCodegen();
+    codegen.additionalProperties().put(CodegenConstants.HIDE_GENERATION_TIMESTAMP, false);
+    codegen.processOpts();
+
+    Assert.assertEquals(codegen.additionalProperties().get(CodegenConstants.HIDE_GENERATION_TIMESTAMP), Boolean.FALSE);
+    Assert.assertEquals(codegen.isHideGenerationTimestamp(), false);
+  }
+
 }
