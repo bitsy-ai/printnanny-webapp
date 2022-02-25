a806d97a8a17c7f09ce9cabffce5ea79b76c30a7
Leigh Johnson <leigh@bitsy.ai>
2022-02-24 22:40:20 -0800

    fix linter errors

--- print_nanny_webapp/devices/services.py
+++ print_nanny_webapp/devices/services.py
@@ -3,14 +3,14 @@ import logging
 from typing import Tuple
 from google.cloud import iot_v1 as cloudiot_v1
 import google.api_core.exceptions
-from django.apps import apps
 from django.conf import settings
 from django.template.loader import render_to_string
 
-Device = apps.get_model("devices", "Device")
-CloudiotDevice = apps.get_model("devices", "CloudiotDevice")
-PublicKey = apps.get_model("devices", "PublicKey")
-JanusStream = apps.get_model("devices", "JanusStream")
+from .models import (
+    CloudiotDevice,
+    Device,
+    PublicKey,
+)
 
 logger = logging.getLogger(__name__)
 
