0554d3d914e88030dffd5cae697269842e780f8d
Release Automation <leigh@bitsy.ai>
2022-02-26 19:27:21 -0800

    install referrals app to base config

--- print_nanny_webapp/referrals/apps.py
+++ print_nanny_webapp/referrals/apps.py
@@ -1,6 +1,18 @@
+import logging
 from django.apps import AppConfig
+
+logger = logging.getLogger(__name__)
 
 
 class ReferralsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "referrals"
+    name = "print_nanny_webapp.referrals"
+
+    def ready(self) -> None:
+        try:
+            import print_nanny_webapp.events.signals  # noqa F401
+        except ImportError:
+            logger.warning(
+                "Failed import print_nanny_webapp.events.signals - no signals will be registered for app=%s",
+                self,
+            )
