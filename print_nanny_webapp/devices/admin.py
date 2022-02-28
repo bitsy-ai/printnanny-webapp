9346ad467ec53b00ed34c410831e8a2f9367d32f
Leigh Johnson <releases@bitsy.ai>
2022-02-28 11:19:19 -0800

    implement admin views for JanusStream, JanusAuth, Devices (#549)

--- print_nanny_webapp/devices/admin.py
+++ print_nanny_webapp/devices/admin.py
@@ -1,1 +1,35 @@
 from django.contrib import admin
+from print_nanny_webapp.devices.models import Device, JanusStream, JanusAuth
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("hostname", "user", "created_dt")
+    model = Device
+
+
+@admin.register(JanusStream)
+class JanusStreamAdmin(admin.ModelAdmin):
+    list_display = (
+        "device",
+        "device_hostname",
+        "active",
+        "info",
+        "config_type",
+        "created_dt",
+    )
+    model = JanusStream
+
+    @admin.display(ordering="device__hostname", description="Device Hostname")
+    def device_hostname(self, obj):
+        return obj.device.hostname
+
+
+@admin.register(JanusAuth)
+class JanusAuthAdmin(admin.ModelAdmin):
+    list_display = ("user", "user_email", "config_type", "created_dt")
+    model = JanusAuth
+
+    @admin.display(ordering="user__email", description="User Email")
+    def user_email(self, obj):
+        return obj.user.email
--- print_nanny_webapp/referrals/admin.py
+++ print_nanny_webapp/referrals/admin.py
@@ -9,24 +9,14 @@ from print_nanny_webapp.referrals.models import ReferralCode, ReferralInvite
 # Register your models here.
 
 
-def generate_referral_qrcode(modeladmin, request, queryset):
-    pass
-
-
-def send_referral_email(modeladmin, request, queryset):
-    pass
-
-
 @admin.register(ReferralCode)
 class ReferralCodeAdmin(admin.ModelAdmin):
     add_form = ReferralCodeCreateForm
     model = ReferralCode
 
-    actions = [generate_referral_qrcode]
     change_form_template = "qrcode_change_form.html"
 
 
 class ReferralInviteAdmin(admin.ModelAdmin):
     add_form = ReferralInviteCreateForm
     model = ReferralInvite
-    actions = [send_referral_email]
