5731fda5bf8daaf9d86d0be5464c68233021286a
Release Automation <leigh@bitsy.ai>
2022-02-28 20:58:32 -0800

    use python9 installer in gh action

---
+++ .envs/.test/.env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export DATABASE_URL="postgres://debug:debug@postgres:5432/print_nanny"
+export COTURN_SECRET_KEY="testsecret1234"
+export COTURN_REALM="turn.test.com"
+export COTURN_DATABASE_URL="postgres://debug:debug@postgres:5432/coturn"
+export REDIS_URL=redis://redis:6379/0
+export CELERY_BROKER_URL=redis://redis:6379/0
+export MAILGUN_API_KEY=""
+export MAILGUN_DOMAIN=""
+export DISCORD_TOKEN=""
+export HONEYCOMB_API_KEY=""
+export CELERY_FLOWER_USER=debug
+export CELERY_FLOWER_PASSWORD=debug
+export LOGLEVEL=debug
