d0ee4c24e7b6eab0bf1b7558b4e17b0dd1eda307
Release Automation <leigh@bitsy.ai>
2022-03-02 11:07:18 -0800

    first pass on templated namespaced ingress

--- k8s/templates/apply.sh
+++ k8s/templates/apply.sh
@@ -5,6 +5,7 @@ set -u
 kubectl apply -f dist/k8s/ns.yml
 kubectl apply -f dist/k8s/cluster-issuer.yml
 kubectl apply -f dist/k8s/cert.yml
+kubectl apply -f dist/k8s/ingress.yml
 kubectl apply -f dist/k8s/ara.yml
 kubectl apply -f dist/k8s/django.yml
 kubectl apply -f dist/k8s/janus.yml
