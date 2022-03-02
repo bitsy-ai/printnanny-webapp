7d7cdfd70fd046332a8068148ee14f6cfd6bf224
Release Automation <leigh@bitsy.ai>
2022-03-01 22:56:05 -0800

    rm standalone cloudsql-proxy for now

--- k8s/templates/apply.sh
+++ k8s/templates/apply.sh
@@ -4,7 +4,6 @@ set -u
 
 kubectl apply -f dist/k8s/ns.yml
 kubectl apply -f dist/k8s/ara.yml
-kubectl apply -f dist/k8s/cloud-sql-proxy.yml
 kubectl apply -f dist/k8s/cluster-issuer.yml
 kubectl apply -f dist/k8s/django.yml
 kubectl apply -f dist/k8s/janus.yml
