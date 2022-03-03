80d5fbea5261d261b4577e95f26e38565b914739
Leigh Johnson <releases@bitsy.ai>
2022-03-02 20:35:45 -0800

    Namespaced ingress for printnanny.ai, live.printnanny.ai, beta.printnanny.ai (#570)
    * first pass on templated namespaced ingress
    
    * adjust static ip
    
    * fix configmap rendering
    
    * fix missing loglevel
    
    * conditionally enable base printnanny.ai cert/dns for live namespace
    
    * enable debuglines
    
    * fix dupe ingress host
    
    * fix neewline

--- k8s/templates/apply.sh
+++ k8s/templates/apply.sh
@@ -5,6 +5,8 @@ set -u
 kubectl apply -f dist/k8s/ns.yml
 kubectl apply -f dist/k8s/cluster-issuer.yml
 kubectl apply -f dist/k8s/cert.yml
+kubectl apply -f dist/k8s/configmap.yml
+kubectl apply -f dist/k8s/ingress.yml
 kubectl apply -f dist/k8s/ara.yml
 kubectl apply -f dist/k8s/django.yml
 kubectl apply -f dist/k8s/janus.yml
--- k8s/templates/render.sh
+++ k8s/templates/render.sh
@@ -8,6 +8,7 @@ j2 k8s/templates/ns.j2 -o dist/k8s/ns.yml
 j2 k8s/templates/configmap.j2 -o dist/k8s/configmap.yml
 j2 k8s/templates/cluster-issuer.j2 -o dist/k8s/cluster-issuer.yml
 j2 k8s/templates/cert.j2 -o dist/k8s/cert.yml
+j2 k8s/templates/ingress.j2 -o dist/k8s/ingress.yml
 j2 k8s/templates/ara.j2 -o dist/k8s/ara.yml
 j2 k8s/templates/django.j2 -o dist/k8s/django.yml
 j2 k8s/templates/janus.j2 -o dist/k8s/janus.yml
