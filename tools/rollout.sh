55b78d5814423ea6095d2f0a9629f90fc8d35ecc
Leigh Johnson <releases@bitsy.ai>
2022-03-03 21:26:19 -0800

    fix missing GCP_SERVICE_ACCOUNT key (#581)
    * fix missing GCP_SERVICE_ACCOUNT key
    
    * fix key
    
    * upgrade node 15
    
    * add namespaced deploy target for gh action
    
    * fix target
    
    * clean  up unused pipelines
    
    * typo
    
    * fix dependencies
    
    * create rollout script

---
+++ tools/rollout.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker tag "print_nanny_webapp:${GIT_SHA}" \
+    us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:latest
+
+docker tag "print_nanny_webapp:${GIT_SHA}" \
+    "us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}"
+
+docker push us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:latest
+docker push "us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}"
+
+
+kubectl -n "$PRINTNANNY_NAMESPACE" set image deployment/ara "ara=us.gcr.io/${GCP_PROJECT}/ara:${GIT_SHA}" --record
+kubectl  -n "$PRINTNANNY_NAMESPACE" set image deployment/django "django=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" --record
+ATTEMPTS=0
+MAX_ATTEMPTS=30
+SLEEP=10
+DEPLOYMENT="deployment/django"
+ROLLOUT_STATUS_CMD="kubectl rollout status $DEPLOYMENT -n $PRINTNANNY_NAMESPACE"
+until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
+  $ROLLOUT_STATUS_CMD
+  ATTEMPTS=$((ATTEMPTS + 1))
+  echo "Waiting $SLEEP sec for $DEPLOYMENT rollout to complete"
+  sleep $SLEEP
+done
