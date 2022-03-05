43374c8ebc04dee448430b063d387756b763aa7d
Release Automation <leigh@bitsy.ai>
2022-03-04 23:03:28 -0800

    fix static url

--- config/settings/base.py
+++ config/settings/base.py
@@ -169,7 +169,7 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = env("DJANGO_STATIC_URL", default="/")
+STATIC_URL = env("DJANGO_STATIC_URL", default="static/")
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 BASE_URL = env("DJANGO_BASE_URL", default="/")
 WS_BASE_URL = env("DJANGO_WS_URL", default="/ws")
