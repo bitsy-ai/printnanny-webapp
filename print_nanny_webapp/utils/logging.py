c1903c615b12d2c7849c23fa8cc5b6b7bfedf048
Release Automation <leigh@bitsy.ai>
2022-03-06 10:37:57 -0800

    fix endpoint

--- print_nanny_webapp/utils/logging.py
+++ print_nanny_webapp/utils/logging.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 class ExcludeHealthEndpoint(logging.Filter):
     def filter(self, record):
-        return record.getMessage().find("/healthcheck") == -1
+        return record.getMessage().find("/health") == -1
 
 
 logging.getLogger("uvicorn.access").addFilter(ExcludeHealthEndpoint())
