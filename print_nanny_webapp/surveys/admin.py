295fcb3b67139db895bb942d723e1dce56d5386e
Release Automation <leigh@bitsy.ai>
2022-03-12 23:57:38 -0800

    create admin view for RemoteAccessSurvey1 survey

--- print_nanny_webapp/surveys/admin.py
+++ print_nanny_webapp/surveys/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from print_nanny_webapp.surveys.models import RemoteAccessSurvey1
 
-# Register your models here.
+
+@admin.register(RemoteAccessSurvey1)
+class RemoteAccessSurvey1Admin(admin.ModelAdmin):
+    list_display = ("email", "first_name", "last_name", "created_dt")
+    model = RemoteAccessSurvey1
