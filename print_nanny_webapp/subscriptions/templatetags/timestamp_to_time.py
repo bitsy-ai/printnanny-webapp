3f32392ab497fc11f9dca9eb286f2bb7f1396f07
Release Automation <leigh@bitsy.ai>
2022-03-15 23:12:14 -0700

    finish first pass

---
+++ print_nanny_webapp/subscriptions/templatetags/timestamp_to_time.py
@@ -0,0 +1,9 @@
+import datetime
+from django import template
+
+register = template.Library()
+
+
+@register.filter("timestamp_to_time")
+def convert_timestamp_to_time(timestamp):
+    return datetime.date.fromtimestamp(int(timestamp))
