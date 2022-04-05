7b875bd46606b35eb274fe2ccd2e8ea1e2794905
Release Automation <leigh@bitsy.ai>
2022-04-05 15:19:19 -0700

    run typescript linter only on .ts and .tsx files

--- print_nanny_vue/.eslintrc.js
+++ print_nanny_vue/.eslintrc.js
@@ -3,7 +3,7 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: "@babel/eslint-parser",
   },
   env: {
     browser: true,
@@ -13,14 +13,11 @@ module.exports = {
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'standard'
   ],
   // required to lint *.vue files
   plugins: [
-    'vue',
-    '@typescript-eslint'
+    'vue'
   ],
   // add your custom rules here
   rules: {
@@ -28,5 +25,34 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+  },
+  overrides: [{
+    files: ["*.ts", "*.tsx"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+
+    // If need to support jsx
+    //     parserOptions: {
+    //       ecmaFeatures: { jsx: true }
+    //     },
+
+    /**
+     * Typescript Rules
+     * https://github.com/bradzacher/eslint-plugin-typescript
+     * Enable your own typescript rules.
+     */
+    rules: {
+      // Prevent TypeScript-specific constructs from being erroneously flagged as unused
+      '@typescript-eslint/no-unused-vars': 'error',
+      // Require PascalCased class and interface names
+      '@typescript-eslint/class-name-casing': 'error',
+      // Require a specific member delimiter style for interfaces and type literals
+      // Default Semicolon style
+      '@typescript-eslint/member-delimiter-style': 'error',
+      // Require a consistent member declaration order
+      '@typescript-eslint/member-ordering': 'error',
+      // Require consistent spacing around type annotations
+      '@typescript-eslint/type-annotation-spacing': 'error',
+    },
+  }]
 }
