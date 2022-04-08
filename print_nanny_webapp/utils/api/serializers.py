28d59e858b33ed5005e3b8443962b19ad0bb21ed
Release Automation <leigh@bitsy.ai>
2022-04-08 12:48:18 -0700

    serialize ApiConfig.static ApiConfig.ws urls

--- print_nanny_webapp/utils/api/serializers.py
+++ print_nanny_webapp/utils/api/serializers.py
@@ -1,5 +1,5 @@
-import json
 from rest_framework import serializers
+from django.conf import settings
 
 
 class ErrorDetailSerializer(serializers.Serializer):
@@ -10,3 +10,12 @@ class ErrorDetailSerializer(serializers.Serializer):
 class PrintNannyApiConfigSerializer(serializers.Serializer):
     bearer_access_token = serializers.CharField(read_only=True)
     base_path = serializers.CharField(read_only=True)
+    static = serializers.SerializerMethodField(read_only=True)
+    ws = serializers.SerializerMethodField(read_only=True)
+
+    def get_static(self, _obj) -> str:
+        absolute_url = self.request.build_absolute_uri(settings.WS_BASE_URL)
+        return absolute_url
+
+    def get_ws(self, _obj) -> str:
+        return self.request.build_absolute_uri(settings.WS_BASE_URL)
