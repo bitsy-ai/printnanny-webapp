3c68bb87110461ad00792a9e0deca8fa676031c4
Release Automation <leigh@bitsy.ai>
2022-04-08 18:21:29 -0700

    rm ws_url

--- print_nanny_webapp/utils/api/serializers.py
+++ print_nanny_webapp/utils/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from django.conf import settings
 
 
 class ErrorDetailSerializer(serializers.Serializer):
@@ -10,17 +9,4 @@ class ErrorDetailSerializer(serializers.Serializer):
 class PrintNannyApiConfigSerializer(serializers.Serializer):
     bearer_access_token = serializers.CharField(read_only=True)
     base_path = serializers.CharField(read_only=True)
-    static = serializers.SerializerMethodField(read_only=True)
-    ws = serializers.SerializerMethodField(read_only=True)
-
-    def get_static(self, _obj) -> str:
-        request = self.context.get("request")
-        if request:
-            return request.build_absolute_uri(settings.STATIC_URL)
-        return settings.STATIC_URL
-
-    def get_ws(self, _obj) -> str:
-        request = self.context.get("request")
-        if request:
-            return request.build_absolute_uri(settings.WS_BASE_URL)
-        return settings.WS_BASE_URL
+    static_url = serializers.CharField(read_only=True)
