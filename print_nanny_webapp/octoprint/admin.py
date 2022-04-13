5a6f6ea3242883ceec892df8f89ecc3fea854b6f
Release Automation <leigh@bitsy.ai>
2022-04-13 11:51:57 -0700

    add created_dt / updated_dt fields to OctoPrintSettings

--- print_nanny_webapp/octoprint/admin.py
+++ print_nanny_webapp/octoprint/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from print_nanny_webapp.octoprint.models import (
+    OctoPrintBackup,
+    OctoPrintInstall,
+    OctoPrinterProfile,
+    OctoPrintSettings,
+    GcodeFile,
+)
 
-# Register your models here.
+
+@admin.register(OctoPrintBackup)
+class OctoPrintBackupAdmin(admin.ModelAdmin):
+    list_display = ("name", "user", "created_dt")
+    model = OctoPrintBackup
+
+
+@admin.register(OctoPrintInstall)
+class OctoPrintInstallAdmin(admin.ModelAdmin):
+    list_display = (
+        "device",
+        "user",
+        "octoprint_version",
+        "printnanny_plugin_version",
+        "created_dt",
+        "updated_dt",
+    )
+    model = OctoPrintInstall
+
+
+# @admin.register(OctoPrinterProfile)
+# class OctoPrinterProfileAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "user",
+#         "name",
+#         "printnanny_plugin_version",
+#         "created_dt",
+#         "updated_dt",
+#     )
+#     model = OctoPrinterProfile
