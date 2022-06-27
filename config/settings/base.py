a08fd3b45eb9705c12730db1a82ac1a0f70b9e94
Leigh Johnson <leigh@bitsy.ai>
2022-06-27 11:56:34 -0700

    fix install guide url

--- config/settings/base.py
+++ config/settings/base.py
@@ -562,7 +562,6 @@ ROADMAP_URL = (
 )
 HELP_OCTOPRINT_PLUGIN_SETUP = "https://help.print-nanny.com/octoprint-plugin-setup/"
 HELP_WIKI = "https://www.notion.so/bitsy-ai/Print-Nanny-Guides-Support-ac1079fafc944d769aa21cf8bffe4692"
-HELP_GETTING_STARTED = "https://bitsy-ai.notion.site/Getting-Started-with-Print-Nanny-817bc65297ff44a085120c663dced5f3"
 HELP_DEVICE_PKI = "https://www.notion.so/bitsy-ai/Securing-IoT-Devices-with-PKI-f9e728fb1e734e6d95efb930d9f23620"
 ERROR_ACTIVATE_LICENSE = "https://www.notion.so/bitsy-ai/License-Activation-Failed-99bb98ebc01546979717da214d2198c8"
 HELP_BOOK_30M = "https://calendly.com/bitsy-ai-labs/30min"
