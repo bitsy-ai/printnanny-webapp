d776ec18df51604ce7cb0f4f2bc7bca31b598499
Leigh Johnson <leigh@bitsy.ai>
2022-07-10 14:40:14 -0700

    also disable broken link middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -160,7 +160,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
