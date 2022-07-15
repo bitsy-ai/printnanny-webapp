791b285982623ad2e0ba893a864f1bf60c58b92a
Leigh Johnson <leigh@bitsy.ai>
2022-07-14 22:57:21 -0700

    redirect to dashboard if already logged in

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
+import { useAccountStore } from "@/stores/account";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,12 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (Login.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      redirect: to => {
+        const account = useAccountStore();
+        if (account.isAuthenticated) {
+          return { path: '/dashboard' }
+        }
+      },
       component: LoginView,
     },
     {
