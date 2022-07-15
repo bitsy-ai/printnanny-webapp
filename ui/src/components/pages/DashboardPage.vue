f325a808fd6d0cfd5b904f66fd57c2e71fe032c9
Leigh Johnson <leigh@bitsy.ai>
2022-07-15 15:59:43 -0700

    fix pinned devices dupe

--- ui/src/components/pages/DashboardPage.vue
+++ ui/src/components/pages/DashboardPage.vue
@@ -153,49 +153,6 @@
             </router-link>
           </div>
         </div>
-        <!-- Pinned devices -->
-        <div class="px-4 mt-6 sm:px-6 lg:px-8">
-          <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Pinned Projects</h2>
-          <ul role="list" class="grid grid-cols-1 gap-4 sm:gap-6 sm:grid-cols-2 xl:grid-cols-4 mt-3">
-            <li v-for="project in pinnedProjects" :key="project.id" class="relative col-span-1 flex shadow-sm rounded-md">
-              <div :class="[project.bgColorClass, 'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md']">
-                {{ project.initials }}
-              </div>
-              <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                <div class="flex-1 px-4 py-2 text-sm truncate">
-                  <a href="#" class="text-gray-900 font-medium hover:text-gray-600">
-                    {{ project.title }}
-                  </a>
-                  <p class="text-gray-500">{{ project.totalMembers }} Members</p>
-                </div>
-                <Menu as="div" class="flex-shrink-0 pr-2">
-                  <MenuButton class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
-                    <span class="sr-only">Open options</span>
-                    <DotsVerticalIcon class="w-5 h-5" aria-hidden="true" />
-                  </MenuButton>
-                  <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                    <MenuItems class="z-10 mx-3 origin-top-right absolute right-10 top-3 w-48 mt-1 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none">
-                      <div class="py-1">
-                        <MenuItem v-slot="{ active }">
-                          <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">View</a>
-                        </MenuItem>
-                      </div>
-                      <div class="py-1">
-                        <MenuItem v-slot="{ active }">
-                          <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Removed from pinned</a>
-                        </MenuItem>
-                        <MenuItem v-slot="{ active }">
-                          <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Share</a>
-                        </MenuItem>
-                      </div>
-                    </MenuItems>
-                  </transition>
-                </Menu>
-              </div>
-            </li>
-          </ul>
-        </div>
-
       <!-- Pinned devices (v-slot can be replaced with #pinned )-->
       <slot name="pinned">
         <PinnedDevices />
