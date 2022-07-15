38a99c109e2839bf281db172e1cdebcbbe42a24e
Leigh Johnson <leigh@bitsy.ai>
2022-07-15 16:01:27 -0700

    move search into separate component

--- ui/src/components/pages/DashboardPage.vue
+++ ui/src/components/pages/DashboardPage.vue
@@ -59,16 +59,8 @@
       <div class="pt-6 h-0 flex-1 flex flex-col overflow-y-auto">
         <!-- User account dropdown -->
         <ProfileMenu />
-        <!-- Sidebar Search -->
-        <div class="px-3 mt-5">
-          <label for="search" class="sr-only">Search</label>
-          <div class="mt-1 relative rounded-md shadow-sm">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none" aria-hidden="true">
-              <SearchIcon class="mr-3 h-4 w-4 text-gray-400" aria-hidden="true" />
-            </div>
-            <input type="text" name="search" id="search" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-9 sm:text-sm border-gray-300 rounded-md" placeholder="Search" />
-          </div>
-        </div>
+        <!-- TODO Sidebar Search -->
+        
         <!-- Sidebar Navigation -->
         <SidebarNav />
       </div>
