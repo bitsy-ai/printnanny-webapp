e68a1babae2f9d5491f1b2b02af8e4db35db17b5
Leigh Johnson <leigh@bitsy.ai>
2022-07-16 12:56:08 -0700

    add termly terms of service

---
+++ ui/src/views/TermsOfServiceView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TermsOfService from "@/components/legal/TermsOfService.vue";
+import FooterNav from "@/components/nav/FooterNav.vue";
+</script>
+
+<template>
+  <main>
+    <TermsOfService />
+    <FooterNav />
+  </main>
+</template>
