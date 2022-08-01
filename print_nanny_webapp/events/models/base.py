e60eef11d04c5cefc39b1d802611db0f5c5beccc
Leigh Johnson <releases@bitsy.ai>
2022-07-31 22:40:52 -0700

    migrate v0 -> v1 event models (#822)
    * drop v0 events models, initialize v1 models
    
    * use compatible stubs package
    
    * fix mypy

---
+++ print_nanny_webapp/events/models/base.py
@@ -0,0 +1,43 @@
+from polymorphic.models import PolymorphicModel
+from safedelete.models import SafeDeleteModel, SOFT_DELETE
+from django.contrib.auth import get_user_model
+from django.db import models
+from .enum import EventSource
+
+
+User = get_user_model()
+
+
+class AbstractEvent(PolymorphicModel, SafeDeleteModel):
+    """
+    Polymorphic Base Event
+    """
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_dt"]
+        index_together = [["source", "subject", "created_dt"]]
+
+    _safedelete_policy = SOFT_DELETE
+    created_dt = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=32, choices=EventSource.choices)
+    subject = models.CharField(max_length=255)
+    payload = models.JSONField(default=dict)
+
+
+class AbstractUserEvent(AbstractEvent):
+    class Meta:
+        abstract = True
+        ordering = ["-created_dt"]
+        index_together = [["user", "source", "subject", "created_dt"]]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # type: ignore[var-annotated]
+
+
+class AbstractPiEvent(AbstractEvent):
+    class Meta:
+        abstract = True
+        ordering = ["-created_dt"]
+        index_together = [["pi", "source", "subject", "created_dt"]]
+
+    pi = models.ForeignKey("devices.Pi", on_delete=models.CASCADE)
--- print_nanny_webapp/events/signals.py
+++ print_nanny_webapp/events/signals.py
@@ -1,39 +1,39 @@
-import logging
-from typing import Callable, Dict, Tuple, Any
+# import logging
+# from typing import Callable, Dict, Tuple, Any
 
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from print_nanny_webapp.events.models import Event
-from .services import (
-    broadcast_event,
-    webrtc_stream_start,
-    webrtc_stream_stop,
-)
-from .enum import WebRTCEventName, WebRTCCommandName
+# from django.dispatch import receiver
+# from django.db.models.signals import post_save
+# from print_nanny_webapp.events.models import Event
+# from .services import (
+#     broadcast_event,
+#     webrtc_stream_start,
+#     webrtc_stream_stop,
+# )
+# from .models.enum import WebRTCEventName, WebRTCCommandName
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-created_handlers: Dict[str, Callable[..., Any]] = {
-    WebRTCCommandName.STREAM_START.value: webrtc_stream_start,
-    WebRTCCommandName.STREAM_STOP.value: webrtc_stream_stop,
-    WebRTCEventName.STREAM_START_ERROR.value: broadcast_event,
-    WebRTCEventName.STREAM_START_SUCCESS.value: broadcast_event,
-}
+# created_handlers: Dict[str, Callable[..., Any]] = {
+#     WebRTCCommandName.STREAM_START.value: webrtc_stream_start,
+#     WebRTCCommandName.STREAM_STOP.value: webrtc_stream_stop,
+#     WebRTCEventName.STREAM_START_ERROR.value: broadcast_event,
+#     WebRTCEventName.STREAM_START_SUCCESS.value: broadcast_event,
+# }
 
 
-@receiver(post_save, dispatch_uid="event_handler")
-def handle_event(sender, instance, created, **kwargs):
-    if created is True and isinstance(instance, Event):
-        handler = created_handlers.get(instance.event_name)
-        logger.info(
-            "events.signals.handle_event calling handler %s with instance %s event_type %s",
-            handler,
-            instance,
-            instance.model,
-        )
+# @receiver(post_save, dispatch_uid="event_handler")
+# def handle_event(sender, instance, created, **kwargs):
+#     if created is True and isinstance(instance, Event):
+#         handler = created_handlers.get(instance.event_name)
+#         logger.info(
+#             "events.signals.handle_event calling handler %s with instance %s event_type %s",
+#             handler,
+#             instance,
+#             instance.model,
+#         )
 
-        if handler is None:
-            logger.debug("No handler configured for event_type=%s in events.signals")
-            return
-        else:
-            return handler(instance)
+#         if handler is None:
+#             logger.debug("No handler configured for event_type=%s in events.signals")
+#             return
+#         else:
+#             return handler(instance)
