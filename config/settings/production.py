d576813fbd6b6cfcca52c26d99db25ab298df18c
Leigh Johnson <leigh@bitsy.ai>
2022-08-01 09:45:17 -0700

    add api.printnanny.ai to CORS allowed origin list

--- config/settings/production.py
+++ config/settings/production.py
@@ -53,7 +53,9 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#csrf-cookie-domain
 CSRF_COOKIE_DOMAIN = env("DJANGO_CSRF_COOKIE_DOMAIN", default=".printnanny.ai")
-CSRF_TRUSTED_ORIGINS = [".printnanny.ai"]
+CSRF_TRUSTED_ORIGINS = ["api.printnanny.ai", "printnanny.ai"]
+CSRF_USE_SESSIONS = True
+
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
@@ -224,6 +226,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://beta.printnanny.ai",
     "https://www.beta.printnanny.ai",
     "https://cdn.printnanny.ai",
+    "https://api.printnanny.ai",
+    "https://api.live.printnanny.ai",
 ]
 
 # posthog
