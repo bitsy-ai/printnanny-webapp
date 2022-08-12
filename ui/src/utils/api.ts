cc18b426c19d9936166cebad731263d58038195e
Leigh Johnson <leigh@bitsy.ai>
2022-08-12 08:26:18 -0700

    Revert "fix csrf header name"
    This reverts commit 021b81ef03e91578ad4f0245633dd2dc6c373efd.

--- ui/src/utils/api.ts
+++ ui/src/utils/api.ts
@@ -5,7 +5,7 @@ const ApiConfig = new api.Configuration({
   basePath: import.meta.env.VITE_PRINTNANNY_API_URL,
   baseOptions: {
     xsrfCookieName: "csrftoken",
-    xsrfHeaderName: "HTTP-X-CSRFTOKEN",
+    xsrfHeaderName: "X-CSRFTOKEN",
     withCredentials: true,
   },
 });
