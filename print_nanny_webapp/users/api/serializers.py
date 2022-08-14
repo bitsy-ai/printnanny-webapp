605ed0509e8fbd52c5d69fbb3d6a66a7ffe82b37
Leigh Johnson <leigh@bitsy.ai>
2022-08-14 15:38:36 -0700

    fix formatting

--- print_nanny_webapp/users/api/serializers.py
+++ print_nanny_webapp/users/api/serializers.py
@@ -1,17 +1,49 @@
-from django.contrib.auth import get_user_model
+import re
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.apps import apps
 
-from django_nats_nkeys.services import nsc_generate_creds
 from django_nats_nkeys.models import NatsOrganizationUser
 from print_nanny_webapp.users.models import EmailWaitlist
 
+
 User = get_user_model()
+MemberBadge = apps.get_model("subscriptions", "MemberBadge")
+
+
+class MemberBadgeSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+
+    def get_type(self, obj) -> str:
+        """
+        splits MemberBadge.type on capital leaders
+        """
+        return " ".join(re.findall("[A-Z][^A-Z]*", obj.type))
+
+    label = serializers.SerializerMethodField()
+
+    def get_label(self, obj) -> str:
+        return obj.label
+
+    class Meta:
+        model = MemberBadge
+        fields = ("id", "created_dt", "updated_dt", "type", "label", "user")
 
 
 class UserSerializer(serializers.ModelSerializer):
+    member_badges = MemberBadgeSerializer(many=True)
+
     class Meta:
         model = User
-        fields = ["email", "id", "first_name", "last_name", "is_beta_tester"]
+        fields = [
+            "email",
+            "id",
+            "first_name",
+            "last_name",
+            "is_beta_tester",
+            "member_badges",
+        ]
+        depth = 1
 
 
 class EmailWaitlistSerializer(serializers.ModelSerializer):
