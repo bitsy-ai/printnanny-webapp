bf166f3eaadaddc830f49a64f2d8836cb7411095
Leigh Johnson <leigh@bitsy.ai>
2022-08-15 16:50:17 -0700

    replace configmap instead of applying

--- k8s/templates/apply.sh
+++ k8s/templates/apply.sh
@@ -5,7 +5,7 @@ set -u
 kubectl apply -f dist/k8s/ns.yml
 kubectl apply -f dist/k8s/cluster-issuer.yml
 kubectl apply -f dist/k8s/cert.yml
-kubectl apply -f dist/k8s/configmap.yml
+kubectl replace-f dist/k8s/configmap.yml
 kubectl apply -f dist/k8s/django.yml
 kubectl apply -f dist/k8s/janus.yml
 kubectl apply -f dist/k8s/gcsfuse.yml
