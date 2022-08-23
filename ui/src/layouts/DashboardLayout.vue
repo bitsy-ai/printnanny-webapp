e6a2ee34eac7db6e9acb0779ce53940df7dea1a8
Leigh Johnson <leigh@bitsy.ai>
2022-08-22 20:57:18 -0700

    add min-h-full to main content element

--- ui/src/layouts/DashboardLayout.vue
+++ ui/src/layouts/DashboardLayout.vue
@@ -114,7 +114,7 @@
                 </div>
               </div>
             </div>
-            <main class="flex-1">
+            <main class="flex-1 min-h-full">
               <!-- Page title & actions -->
               <div
                 class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
