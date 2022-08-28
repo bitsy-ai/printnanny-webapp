7e892c4122fcd8f20a8a9290a5580c53902af9b2
Leigh Johnson <leigh@bitsy.ai>
2022-08-27 17:03:49 -0700

    show error uuid instead, handle janus

--- ui/src/utils/api.ts
+++ ui/src/utils/api.ts
@@ -23,7 +23,25 @@ function handleApiError(e: Error | AxiosError) {
   const header = "Oops, unexpected error";
 
   if (axios.isAxiosError(e)) {
-    message = JSON.stringify(e)
+    const container = { message: "", error_uuid: "" };
+    if (
+      e.response?.data.non_field_errors &&
+      e.response?.data.non_field_errors.length > 0
+    ) {
+      message = e.response.data.non_field_errors.join("\n");
+      container["message"] = e.response.data.non_field_errors.join("\n");
+    } else if (e.response?.data.detail) {
+      container["message"] = e.response.data.detail;
+    } else if (e.response?.data.error) {
+      container["message"] = e.response.data.error;
+    } else {
+      container["message"] = e.response?.data;
+    }
+
+    if (e.response?.data?.error_uuid) {
+      container["error_uuid"] = e.response?.data?.error_uuid
+    }
+    message = JSON.stringify(container, null, 2)
   }
   const alert: UiAlert = {
     header,
