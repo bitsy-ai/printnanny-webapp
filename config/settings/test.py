430e7ceb014bb2cdee966551479d7a911ebeb853
Leigh Johnson <leigh@bitsy.ai>
2022-10-27 14:37:55 -0700

    fix tests ALLOWED_HOSTS

--- config/settings/test.py
+++ config/settings/test.py
@@ -18,6 +18,12 @@ SECRET_KEY = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
+
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
