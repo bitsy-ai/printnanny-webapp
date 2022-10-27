8541ab50c0b04b8497f07049bb6bbfeccebe048f
Leigh Johnson <leigh@bitsy.ai>
2022-10-27 11:15:40 -0700

    rm MemberBadge from serializer

--- print_nanny_webapp/users/api/serializers.py
+++ print_nanny_webapp/users/api/serializers.py
@@ -1,7 +1,5 @@
-import re
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.apps import apps
 
 from django_nats_nkeys.services import nsc_generate_creds
 from django_nats_nkeys.models import NatsOrganizationUser
@@ -9,26 +7,9 @@ from print_nanny_webapp.users.models import EmailWaitlist
 
 
 User = get_user_model()
-MemberBadge = apps.get_model("subscriptions", "MemberBadge")
-
-
-class MemberBadgeSerializer(serializers.ModelSerializer):
-    type = serializers.SerializerMethodField()
-
-    def get_type(self, obj) -> str:
-        """
-        splits MemberBadge.type on capital leaders
-        """
-        return " ".join(re.findall("[A-Z][^A-Z]*", obj.type))
-
-    class Meta:
-        model = MemberBadge
-        fields = ("id", "created_dt", "updated_dt", "type", "label", "user")
 
 
 class UserSerializer(serializers.ModelSerializer):
-    member_badges = MemberBadgeSerializer(many=True, read_only=True)
-
     class Meta:
         model = User
         fields = [
@@ -37,7 +18,6 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "is_beta_tester",
-            "member_badges",
         ]
         depth = 1
 
