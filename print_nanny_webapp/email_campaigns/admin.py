48bbd9dadfae7ea66f47ee80c21c83c78a6d73bc
Leigh Johnson <leigh@bitsy.ai>
2022-10-27 23:18:32 -0700

    setup send fn

--- print_nanny_webapp/email_campaigns/admin.py
+++ print_nanny_webapp/email_campaigns/admin.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
+
+from print_nanny_webapp.email_campaigns.models import Campaign
+
 # Register your models here.
-
-
-# @admin.register(Campaign)
-# class CampaignAdmin(admin.ModelAdmin):
-#     list_display = ("created_dt", "template", "subject", "send_fn")
-#     model = Campaign
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("created_dt", "template", "subject", "send_fn")
+    model = Campaign
--- print_nanny_webapp/email_campaigns/services.py
+++ print_nanny_webapp/email_campaigns/services.py
@@ -1,10 +1,54 @@
+from typing import Dict
+from django.db.models.query import QuerySet
+
 from anymail.message import AnymailMessage
 
+from print_nanny_webapp.email_campaigns.models import Campaign
+from print_nanny_webapp.users.models import User
 
-def send_fn_founding_member_november_2022_offer():
+
+def format_email(user: User) -> str:
+    result = ""
+    if user.first_name is not None:
+        result += user.first_name
+        result += " "
+    if user.last_name is not None:
+        result += user.last_name
+        result += " "
+    if result == "":
+        return user.email
+    else:
+        return f"{result} <{user.email}>"
+
+
+def format_merge_data(user: User) -> Dict[str, str]:
+    if user.first_name:
+        return dict(name=user.first_name)
+    return dict(name="Maker")
+
+
+def format_merge_metadata(user: User, campaign: Campaign) -> Dict[str, str]:
+    return dict(user_id=str(user.id), campaign_id=str(campaign.id))
+
+
+def send_fn_founding_member_november_2022_offer(
+    queryset: QuerySet[User], campaign: Campaign
+):
+
     msg = AnymailMessage(
-        headers={
-            "X-Example-Header": "myapp",
-        },
+        subject=campaign.subject,
         tags=["marketing", "founding_member"],
+        from_email="beta@mail.printnanny.ai",
     )
+
+    msg.track_clicks = True
+    msg.track_opens = True
+
+    msg.to = [format_email(u) for u in queryset]
+    msg.merge_data = [format_merge_data(u) for u in queryset]
+    msg.merde_metadata = [format_merge_metadata(u, campaign) for u in queryset]
+    msg.esp_extra = {
+        "o:deliverytime-optimize-period": "24h",  # use Mailgun Send Time Optimization
+        "o:time-zone-localize": "16:00",  # use Mailgun Timezone Optimization
+        "h:Reply-To": "leigh@printnanny.ai",
+    }
