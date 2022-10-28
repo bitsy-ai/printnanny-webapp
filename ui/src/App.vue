faa2333d696f803f2165054edf22611735447255
Leigh Johnson <leigh@bitsy.ai>
2022-10-28 12:10:58 -0700

    Disable tidio in dev (#969)
    * disable tidio in dev mode
    
    * comment
    
    * lint

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 // import validator rules so all forms can access global rules
 import "@/utils/validators";
+
+// only enable tidio in production
+const tidioScript = import.meta.env.VITE_TIDIO_SCRIPT_URI;
+if (tidioScript !== undefined) {
+  const el = document.createElement("script");
+  el.setAttribute("src", tidioScript);
+  document.body.appendChild(el);
+}
 </script>
 
-<template>
-  <router-view> </router-view>
-</template>
+<template><router-view> </router-view>></template>
