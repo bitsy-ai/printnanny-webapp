f59c8e06e94c53e16f7632bda1323e3d49425c1e
Leigh Johnson <leigh@bitsy.ai>
2022-11-03 15:20:32 -0700

    Campaign helper fn (#1001)
    * filter already subscribed from campaign helper fns
    
    * fix tests

--- print_nanny_webapp/email_campaigns/management/commands/send_campaign_batch.py
+++ print_nanny_webapp/email_campaigns/management/commands/send_campaign_batch.py
@@ -3,7 +3,7 @@ from django.apps import apps
 from django.contrib.auth import get_user_model
 
 import print_nanny_webapp.email_campaigns.services
-from print_nanny_webapp.email_campaigns.models import Campaign, EmailMessage
+from print_nanny_webapp.email_campaigns.models import Campaign
 
 User = get_user_model()
 
@@ -26,20 +26,7 @@ class Command(BaseCommand):
         model = apps.get_model(app, model_str)
 
         campaign = Campaign.objects.get(template=template)
-
-        already_sent = EmailMessage.objects.filter(campaign=campaign).values("email")
-
-        # import pdb
-
-        # pdb.set_trace()
-
-        emails = (
-            model.objects.exclude(email__in=already_sent)
-            .order_by("-created_dt")
-            .all()[:limit]
-            .values_list("email", flat=True)
-        )
         fn_name = campaign.send_fn.split(".")[-1]
         func = getattr(print_nanny_webapp.email_campaigns.services, fn_name)
 
-        func(list(emails), campaign)
+        func(campaign, model, limit=limit)
--- print_nanny_webapp/email_campaigns/services.py
+++ print_nanny_webapp/email_campaigns/services.py
@@ -1,9 +1,10 @@
 import logging
-from typing import Dict, List, Optional, Union
+from typing import Dict, Optional, Union
 
 from anymail.message import AnymailMessage
+from django.db import models
 
-from print_nanny_webapp.email_campaigns.models import Campaign
+from print_nanny_webapp.email_campaigns.models import Campaign, EmailMessage
 from print_nanny_webapp.users.models import User
 
 logger = logging.getLogger(__name__)
@@ -34,9 +35,54 @@ def format_merge_metadata(email: str) -> Dict[str, Optional[Union[str, int]]]:
 
 
 def send_fn_founding_member_november_2022_offer(
-    emails: List[str], campaign: Campaign
+    campaign: Campaign, model: models.Model, limit=10
+) -> AnymailMessage:
+    already_subscribed = User.objects.all().values("email")
+    already_sent = EmailMessage.objects.filter(campaign=campaign).values("email")
+    emails = list(
+        model.objects.exclude(email__in=already_sent)
+        .exclude(email__in=already_subscribed)
+        .order_by("-created_dt")
+        .all()[:limit]
+        .values_list("email", flat=True)
+    )
+    msg = AnymailMessage(
+        subject=campaign.subject,
+        tags=["marketing", "founding_member"],
+        from_email="PrintNanny <beta@mail.printnanny.ai>",
+    )
+
+    msg.template_id = campaign.template
+
+    msg.to = emails
+    msg.merge_metadata = {email: format_merge_metadata(email) for email in emails}
+    msg.esp_extra = {
+        # TODO: send-time optimization is only available for single recipient messages
+        # "o:deliverytime-optimize-period": "24h",  # use Mailgun Send Time Optimization
+        # "o:time-zone-localize": "16:00",  # use Mailgun Timezone Optimization
+        "h:Reply-To": "leigh@printnanny.ai",
+    }
+    msg.merge_global_data = {"campaign_id": campaign.id}
+
+    logger.info("Starting campaign %s to %s recipients", campaign.template, len(msg.to))
+    msg.send(fail_silently=False)
+    logger.info("Success! Started campaign %s", campaign.template)
+    return msg
+
+
+def send_fn_founding_member_november_2022_followup_offer(
+    campaign: Campaign, model: models.Model, limit=10
 ) -> AnymailMessage:
 
+    already_subscribed = User.objects.all().values("email")
+    already_sent = EmailMessage.objects.filter(campaign=campaign).values("email")
+    emails = list(
+        model.objects.exclude(email__in=already_sent)
+        .exclude(email__in=already_subscribed)
+        .order_by("-created_dt")
+        .all()[:limit]
+        .values_list("email", flat=True)
+    )
     msg = AnymailMessage(
         subject=campaign.subject,
         tags=["marketing", "founding_member"],
