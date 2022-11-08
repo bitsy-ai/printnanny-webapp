4b12f40ecaf79aeb6c9818627a96534d517261e4
Leigh Johnson <leigh@bitsy.ai>
2022-11-08 09:35:22 -0800

    Fix headline mobile size (#1016)
    * fix headlines on mobile
    
    * clean up copy

--- ui/src/components/pages/HomePage.vue
+++ ui/src/components/pages/HomePage.vue
@@ -8,7 +8,7 @@
           >
             <div class="lg:py-12">
               <h1
-                class="tracking-tight font-extrabold text-white sm:text-6xl xl:text-6xl"
+                class="tracking-tight font-extrabold text-white text-4xl xl:text-6xl"
               >
                 <span
                   class="block pb-2 block bg-clip-text text-transparent rounded-md shadow bg-gradient-to-r from-indigo-400 to-violet-500 sm:pb-2"
@@ -21,7 +21,7 @@
                 👀
               </h1>
               <h2
-                class="sm:text-xl md:text-3xl text-white mt-4 mb-4 text-3xl font-extrabold tracking-tight sm:text-4xl"
+                class="sm:text-xl md:text-3xl text-white mt-4 mb-4 text-2xl font-extrabold tracking-tight sm:text-4xl"
               >
                 PrintNanny is the
                 <span
@@ -38,7 +38,7 @@
                   class="transform md-shadow hover:scale-110 ease-in-out delay-150 duration-300 mt-6 block w-full sm:text-xl lg:text-lg xl:text-xl py-3 px-4 rounded-md shadow bg-gradient-to-r from-red-500 to-violet-600 text-white font-medium hover:from-red-600 hover:to-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
                 >
                   Want to skip the waitlist?<br />
-                  Become a Founding Members and get early access.
+                  Founding Members get early access.
                 </button>
               </router-link>
             </div>
