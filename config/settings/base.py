7a917c7a4036ce8013439a3196dbdf4055d598ef
Leigh Johnson <leigh@bitsy.ai>
2022-11-14 13:57:41 -0800

    use noreply@printnanny.ai to send 2fa tokens

--- config/settings/base.py
+++ config/settings/base.py
@@ -665,7 +665,7 @@ INSTALLED_APPS += ["drfpasswordless"]
 PASSWORDLESS_AUTH = {
     "PASSWORDLESS_AUTH_TYPES": ["EMAIL"],
     "PASSWORDLESS_USER_MARK_EMAIL_VERIFIED": True,
-    "PASSWORDLESS_EMAIL_NOREPLY_ADDRESS": "noreply@print-nanny.com",
+    "PASSWORDLESS_EMAIL_NOREPLY_ADDRESS": "noreply@printnanny.ai",
     "PASSWORDLESS_EMAIL_PLAINTEXT_MESSAGE": "Enter this token to sign in: %s",
     "PASSWORDLESS_AUTH_PREFIX": "accounts/2fa-auth/",
     "PASSWORDLESS_VERIFY_PREFIX": "accounts/2fa-auth/verify/",
