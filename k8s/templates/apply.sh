1835b5c5be4711058877834e761d1de64c6d6712
Leigh Johnson <leigh@bitsy.ai>
2022-12-22 18:26:15 -0800

    hotfix: discord k8s config

--- k8s/templates/apply.sh
+++ k8s/templates/apply.sh
@@ -6,6 +6,7 @@ kubectl apply -f dist/k8s/ns.yml
 kubectl apply -f dist/k8s/cluster-issuer.yml
 kubectl apply -f dist/k8s/cert.yml
 kubectl apply -f dist/k8s/configmap.yml
+kubectl apply -f dist/k8s/discord.yml
 kubectl apply -f dist/k8s/django.yml
 kubectl apply -f dist/k8s/janus.yml
 kubectl apply -f dist/k8s/gcsfuse.yml
