864fee334fcca091efb4d8e94419b8e70754d9e0
Leigh Johnson <leigh@bitsy.ai>
2022-12-26 12:36:47 -0800

    add extra logging to convert_size

--- print_nanny_webapp/devices/utils.py
+++ print_nanny_webapp/devices/utils.py
@@ -18,11 +18,15 @@ class RTPCapacityException(Exception):
 def convert_size(size_bytes):
     if size_bytes == 0:
         return "0B"
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return "%s %s" % (s, size_name[i])
+    try:
+        size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+        i = math.floor(math.log(size_bytes, 1024))
+        p = math.pow(1024, i)
+        s = round(size_bytes / p, 2)
+        return "%s %s" % (s, size_name[i])
+    except TypeError as e:
+        logger.error("Error in convert_size for size_bytes=%s - %s", size_bytes, e)
+        return "0B"
 
 
 def is_database_synchronized(database):
