834b852f5a104165f1850775fd0584389c9262b3
Leigh Johnson <leigh@bitsy.ai>
2022-12-27 20:58:04 -0800

    add admin panel for EmailTrackingEvent and EmailMessage models

--- print_nanny_webapp/email_campaigns/admin.py
+++ print_nanny_webapp/email_campaigns/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 
-from print_nanny_webapp.email_campaigns.models import Campaign
+from print_nanny_webapp.email_campaigns.models import (
+    Campaign,
+    EmailTrackingEvent,
+    EmailMessage,
+)
 
 # Register your models here.
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
     list_display = ("created_dt", "template", "subject", "send_fn")
     model = Campaign
+
+
+@admin.register(Campaign)
+class EmailTrackingEventAdmin(admin.ModelAdmin):
+    list_display = ("created_dt", "recipient", "email_message", "user", "campaign")
+    model = EmailTrackingEvent
+
+
+@admin.register(Campaign)
+class EmailMessageAdmin(admin.ModelAdmin):
+    list_display = ("message_id", "user", "email", "campsign", "send_status")
+    model = EmailMessage
