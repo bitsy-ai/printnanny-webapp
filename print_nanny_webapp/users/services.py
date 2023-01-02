a374d16414174083cc4c0050456e2f0d0b522257
Leigh Johnson <leigh@bitsy.ai>
2023-01-01 18:49:07 -0800

    fix mypy

--- print_nanny_webapp/users/services.py
+++ print_nanny_webapp/users/services.py
@@ -1,11 +1,10 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AbstractUser
 
 User = get_user_model()
 
 
-def get_or_create_user_by_email(email: str) -> AbstractUser:
+def get_or_create_user_by_email(email: str):
     try:
-        return User.objects.get(email=email)
+        return User.objects.get(email=email)  # type: ignore[has-type]
     except User.DoesNotExist:
-        return User.objects.create_user(email, User.objects.make_random_password())
+        return User.objects.create_user(email, User.objects.make_random_password())  # type: ignore[has-type]
