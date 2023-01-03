e8f09f3a2bb89fce38facf792df822bc00be8660
Leigh Johnson <leigh@bitsy.ai>
2023-01-02 20:27:43 -0800

    log asgi request as well

--- print_nanny_webapp/crash_reports/api/views.py
+++ print_nanny_webapp/crash_reports/api/views.py
@@ -34,15 +34,15 @@ class CrashReportViewSet(
     lookup_field = "id"
 
     parser_classes = [
+        parsers.FormParser,
         parsers.MultiPartParser,
     ]
     permission_classes = (AllowAny,)
 
     def create(self, request, *args, **kwargs):
         logger.info(
-            "CrashReport.create called with request=%s args=%s kwargs%s",
+            "CrashReport.create called with request=%s asgi.request=%s",
             request.__dict__,
-            args,
-            kwargs,
+            request._request.__dict__,
         )
         return super().create(request, *args, **kwargs)
