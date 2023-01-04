8a237be8f250fb0e33c94b84626d392d7460deab
Leigh Johnson <leigh@bitsy.ai>
2023-01-03 17:03:29 -0800

    add GET /api/crash-reports and GET /api/crash-reports/:id

--- print_nanny_webapp/crash_reports/api/views.py
+++ print_nanny_webapp/crash_reports/api/views.py
@@ -2,17 +2,24 @@ import logging
 
 from drf_spectacular.utils import extend_schema_view, extend_schema
 
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import (
+    ListModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
     CreateModelMixin,
+    DestroyModelMixin,
 )
 from rest_framework import parsers
 
+from print_nanny_webapp.utils.api.permissions import IsObjectOwner
 from print_nanny_webapp.crash_reports.models import CrashReport
 from print_nanny_webapp.crash_reports.api.serializers import CrashReportSerializer
 from print_nanny_webapp.utils.api.views import (
     generic_create_errors,
+    generic_list_errors,
 )
 
 logger = logging.getLogger(__name__)
@@ -23,11 +30,20 @@ logger = logging.getLogger(__name__)
         tags=["crash-reports"],
         request=CrashReportSerializer,
         responses={201: CrashReportSerializer} | generic_create_errors,
-    )
+    ),
+    list=extend_schema(
+        tags=["crash-report"],
+        responses={
+            200: CrashReportSerializer(many=True),
+        }
+        | generic_list_errors,
+    ),
 )
 class CrashReportViewSet(
     GenericViewSet,
     CreateModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
 ):
     serializer_class = CrashReportSerializer
     queryset = CrashReport.objects.all()
@@ -37,12 +53,11 @@ class CrashReportViewSet(
         parsers.FormParser,
         parsers.MultiPartParser,
     ]
-    permission_classes = (AllowAny,)
-
-    def create(self, request, *args, **kwargs):
-        logger.info(
-            "CrashReport.create called with request=%s asgi.request=%s",
-            request.__dict__,
-            request._request.__dict__,
+    # allow create from un-authenticated requests, but required authentication and object ownership for list/get
+    def get_permissions(self):
+        if self.action == "create":
+            return (AllowAny,)
+        return (
+            IsAuthenticated,
+            IsObjectOwner,
         )
-        return super().create(request, *args, **kwargs)
