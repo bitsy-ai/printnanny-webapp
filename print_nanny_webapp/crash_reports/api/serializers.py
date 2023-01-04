a7951ed751779108a2b2e31317f62dd1e0612a97
Leigh Johnson <leigh@bitsy.ai>
2023-01-03 17:51:13 -0800

    fix url gettr method

--- print_nanny_webapp/crash_reports/api/serializers.py
+++ print_nanny_webapp/crash_reports/api/serializers.py
@@ -4,7 +4,10 @@ from print_nanny_webapp.crash_reports.models import CrashReport
 
 
 class CrashReportSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name="crash-reports:list")
+    url = serializers.SerializerMethodField()
+
+    def get_url(self, obj) -> str:
+        return f"/crash-reports/{obj.id}/"
 
     class Meta:
         model = CrashReport
