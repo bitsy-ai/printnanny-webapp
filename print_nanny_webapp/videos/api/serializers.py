ab1470e159999db55770035be0c68082863ff385
Leigh Johnson <leigh@bitsy.ai>
2023-01-19 12:02:45 -0800

    fix VideoRecording fieldset to match asyncapi spec (#1101)

--- print_nanny_webapp/videos/api/serializers.py
+++ print_nanny_webapp/videos/api/serializers.py
@@ -4,12 +4,12 @@ from print_nanny_webapp.videos.models import VideoRecording
 
 
 class VideoRecordingSerializer(serializers.ModelSerializer):
-    mjr_upload_url = serializers.SerializerMethodField()
+    mp4_upload_url = serializers.SerializerMethodField()
 
-    def get_mjr_upload_url(self, obj) -> str:
-        return obj.mjr_upload_url()
+    def get_mp4_upload_url(self, obj) -> str:
+        return obj.mp4_upload_url()
 
     class Meta:
         model = VideoRecording
         exclude = ("deleted",)
-        read_only_fields = ("id", "mjr_upload_url", "user")
+        read_only_fields = ("id", "mp4_upload_url", "user")
