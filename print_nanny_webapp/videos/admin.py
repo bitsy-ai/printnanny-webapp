2496ce1bbecae227bdcfd15817f66fe6a6c74fb6
Leigh Johnson <leigh@bitsy.ai>
2023-01-22 15:54:35 -0800

    create VideoRecordingAdmin panel

--- print_nanny_webapp/videos/admin.py
+++ print_nanny_webapp/videos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from print_nanny_webapp.videos.models import VideoRecording
 
 # Register your models here.
+@admin.register(VideoRecording)
+class VideoRecordingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "recording_start",
+        "recording_end",
+        "recording_status",
+        "cloud_sync_start",
+        "cloud_sync_end",
+        "cloud_sync_status",
+    )
+    model = VideoRecording
