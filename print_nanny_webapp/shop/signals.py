138be7801dee0a838ba0b7803dcd1d4fb2e86cb5
Leigh Johnson <leigh@bitsy.ai>
2023-02-03 15:36:40 -0800

    simplify djstripe webhooks behavior

--- print_nanny_webapp/shop/signals.py
+++ print_nanny_webapp/shop/signals.py
@@ -2,7 +2,6 @@ import logging
 import requests
 
 from django.conf import settings
-from djstripe import models
 from djstripe import webhooks
 
 logger = logging.getLogger(__name__)
@@ -10,27 +9,27 @@ logger = logging.getLogger(__name__)
 
 @webhooks.handler("checkout.session.completed")
 def notify_checkout_session_completed(event, **_kwargs):
-    session_id = event.data.get("id")
-    if session_id is None:
-        logger.error("Failed to read event.id from event=%s", event)
-        return
+    # session_id = event.data.get("id")
+    # if session_id is None:
+    #     logger.error("Failed to read event.id from event=%s", event)
+    #     return
 
-    session = models.Session(id=session_id)
-    dashboard_url = session.get_stripe_dashboard_url()
+    # session = models.Session(id=session_id)
+    # dashboard_url = session.get_stripe_dashboard_url()
 
-    body = {"content": f"Stripe checkout.session.completed - {dashboard_url}"}
-    requests.post(settings.DISCORD_NEW_SIGNUP_WEBHOOK, json=body)
+    body = {"content": f"Stripe checkout.session.completed - {event}"}
+    requests.post(settings.DISCORD_NEW_SIGNUP_WEBHOOK, json=body, timeout=30)
 
 
 @webhooks.handler("checkout.session.expired")
 def notify_checkout_session_expired(event, **_kwargs):
-    session_id = event.data.get("id")
-    if session_id is None:
-        logger.error("Failed to read event.id from event=%s", event)
-        return
+    # session_id = event.data.get("id")
+    # if session_id is None:
+    #     logger.error("Failed to read event.id from event=%s", event)
+    #     return
 
-    session = models.Session(id=session_id)
-    dashboard_url = session.get_stripe_dashboard_url()
+    # session = models.Session(id=session_id)
+    # dashboard_url = session.get_stripe_dashboard_url()
 
-    body = {"content": f"Stripe checkout.session.expired- {dashboard_url}"}
-    requests.post(settings.DISCORD_NEW_SIGNUP_WEBHOOK, json=body)
+    body = {"content": f"Stripe checkout.session.expired- {event}"}
+    requests.post(settings.DISCORD_NEW_SIGNUP_WEBHOOK, json=body, timeout=30)
