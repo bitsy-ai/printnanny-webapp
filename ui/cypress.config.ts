bdfdadf186b8c0401d77e2432dda9f1de21fc9c9
Leigh Johnson <leigh@bitsy.ai>
2023-02-06 15:47:23 -0800

    update cypress project id

--- ui/cypress.config.ts
+++ ui/cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "amsn8j",
+  projectId: "7zcv7a",
   chromeWebSecurity: false,
   modifyObstructiveCode: true,
   experimentalModifyObstructiveThirdPartyCode: true,
