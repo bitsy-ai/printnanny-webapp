aec0f46bd47da53052a9c9c2eac2da91aa23c1ac
Leigh Johnson <leigh@bitsy.ai>
2023-02-06 19:12:26 -0800

    delete unused code

--- ui/src/components/nav/FullWidthFlyoutMenu.vue
+++ ui/src/components/nav/FullWidthFlyoutMenu.vue
@@ -151,36 +151,4 @@ const props = defineProps({
   },
 });
 
-const solutions = [
-  {
-    name: "Analytics",
-    description:
-      "Get a better understanding of where your traffic is coming from.",
-    href: "#",
-    icon: ChartBarIcon,
-  },
-  {
-    name: "Engagement",
-    description: "Speak directly to your customers in a more meaningful way.",
-    href: "#",
-    icon: CursorArrowRaysIcon,
-  },
-  {
-    name: "Security",
-    description: "Your customers' data will be safe and secure.",
-    href: "#",
-    icon: ShieldCheckIcon,
-  },
-  {
-    name: "Integrations",
-    description: "Connect with third-party tools that you're already using.",
-    href: "#",
-    icon: Squares2X2Icon,
-  },
-];
-const callsToAction = [
-  { name: "Watch Demo", href: "#", icon: PlayIcon },
-  { name: "View All Products", href: "#", icon: CheckCircleIcon },
-  { name: "Contact Sales", href: "#", icon: PhoneIcon },
-];
 </script>
