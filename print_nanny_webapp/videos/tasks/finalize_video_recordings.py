0249ad126301db710f963b274dc565097dbe6bbb
Leigh Johnson <leigh@bitsy.ai>
2023-03-17 10:37:25 -0700

    hotfix: django.core.exceptions.AppRegistryNotReady

--- print_nanny_webapp/videos/tasks/finalize_video_recordings.py
+++ print_nanny_webapp/videos/tasks/finalize_video_recordings.py
@@ -2,6 +2,7 @@ import logging
 import asyncio
 from datetime import datetime, timedelta
 
+import django
 from django.apps import apps
 from django.db.models import Q
 from asgiref.sync import sync_to_async
@@ -10,10 +11,11 @@ logger = logging.getLogger(__name__)
 
 PERIOD = 30  # run task every n seconds
 FINALIZE_TIME_LIMIT = 20  # time limit on outstanding finalize tasks (minutes)
+django.setup()
+VideoRecordings = apps.get_model("videos", "VideoRecordings")
 
 
 def get_video_recordings_ready_for_finalization():
-    VideoRecordings = apps.get_model("videos", "VideoRecordings")
     time_threshold = datetime.now() - timedelta(minutes=FINALIZE_TIME_LIMIT)
     # return all VideoRecordings where finalization hasn't been started, or that have execeeded FINALIZE_TIME_LIMIT since being started
     return VideoRecordings.objects.filter(
