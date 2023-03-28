4af3498cc884196cae2eaf8fad12075ad41bfc04
Leigh Johnson <leigh@bitsy.ai>
2023-03-27 17:45:17 -0700

    install celery-types package

--- config/celery_app.py
+++ config/celery_app.py
@@ -2,6 +2,11 @@ import os
 
 from celery import Celery
 
+from celery.app.task import Task
+
+Task.__class_getitem__ = classmethod(lambda cls, *args, **kwargs: cls)  # type: ignore[attr-defined]
+
+
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
