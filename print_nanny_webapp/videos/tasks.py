f0e07ab79ebc052612be2a38c0bb6ff4bbde783b
Leigh Johnson <leigh@bitsy.ai>
2023-03-27 18:19:02 -0700

    test celery task: finalize_video_recording (#1194)

---
+++ print_nanny_webapp/videos/tasks.py
@@ -0,0 +1,62 @@
+import logging
+import asyncio
+from datetime import timedelta
+
+import django
+
+from django.utils import timezone
+from django.apps import apps
+from django.db.models import Q
+from asgiref.sync import sync_to_async
+
+from config import celery_app
+
+django.setup()
+logger = logging.getLogger(__name__)
+
+PERIOD = 30  # run task every n seconds
+FINALIZE_TIME_LIMIT = 20  # time limit on outstanding finalize tasks (minutes)
+VideoRecordings = apps.get_model("videos", "VideoRecording")
+
+
+@celery_app.task()
+def finalize_video_recording(video_recording_id: int):
+    video_recording = VideoRecordings.get(id=video_recording_id)
+    logger.info(
+        "Preparing to finalize VideoRecording id=%s count_parts=%s",
+        video_recording.id,
+        video_recording.video_recording_parts_set.count(),
+    )
+
+
+# def get_video_recordings_ready_for_finalization():
+#     time_threshold = timezone.now() - timedelta(minutes=FINALIZE_TIME_LIMIT)
+#     # return all VideoRecordings where finalization hasn't been started, or that have execeeded FINALIZE_TIME_LIMIT since being started
+#     return VideoRecordings.objects.filter(
+#         Q(recording_start=None, cloud_sync_done=True)
+#         | Q(recording_start__lt=time_threshold, cloud_sync_done=True)
+#     )
+
+
+# async def finalize_video_recordings():
+#     get_recordings = sync_to_async(
+#         get_video_recordings_ready_for_finalization, thread_sensitive=True
+#     )
+#     recordings = await get_recordings()
+#     get_count = sync_to_async(recordings.count)
+#     count = await get_count()
+#     if count > 0:
+#         logger.info("Found %i VideoRecordings ready for finalization", count)
+
+
+# async def run_periodically(interval, periodic_function):
+#     while True:
+#         await asyncio.gather(
+#             asyncio.sleep(interval),
+#             periodic_function(),
+#         )
+
+
+# if __name__ == "__main__":
+#     logger.info("Initializing finalize_video_recordings.py worker")
+#     asyncio.run(run_periodically(PERIOD, finalize_video_recordings))
