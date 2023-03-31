29f6d57b8ece129a101a3323f6cd32e7c4dfffe0
Leigh Johnson <leigh@bitsy.ai>
2023-03-31 10:26:12 -0700

    fix: shellchecl

--- tools/rollout.sh
+++ tools/rollout.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
-kubectl  -n "$PRINTNANNY_NAMESPACE" set image deployment/django "django=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" "celery-worker=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" "celery-beat=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" --record
+DEPLOYMENT="deployment/django"
 
-kubectl rollout status $DEPLOYMENT -n $PRINTNANNY_NAMESPACE --wait --timeout=20m
+kubectl  -n "$PRINTNANNY_NAMESPACE" set image "$DEPLOYMENT" "django=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" "celery-worker=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" "celery-beat=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" --record
+
+kubectl rollout status "$DEPLOYMENT" -n "$PRINTNANNY_NAMESPACE" --wait
