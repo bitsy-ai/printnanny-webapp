984ad7606932cd370093bc215081a27f3b96ea4f
Leigh Johnson <leigh@bitsy.ai>
2023-03-31 10:25:23 -0700

    simplify rollout

--- tools/rollout.sh
+++ tools/rollout.sh
@@ -3,14 +3,5 @@
 set -euo pipefail
 
 kubectl  -n "$PRINTNANNY_NAMESPACE" set image deployment/django "django=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" "celery-worker=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" "celery-beat=us.gcr.io/${GCP_PROJECT}/print_nanny_webapp:${GIT_SHA}" --record
-ATTEMPTS=0
-MAX_ATTEMPTS=300
-SLEEP=10
-DEPLOYMENT="deployment/django"
-ROLLOUT_STATUS_CMD="kubectl rollout status $DEPLOYMENT -n $PRINTNANNY_NAMESPACE"
-until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
-  $ROLLOUT_STATUS_CMD
-  ATTEMPTS=$((ATTEMPTS + 1))
-  echo "Waiting $SLEEP sec for $DEPLOYMENT rollout to complete"
-  sleep $SLEEP
-done
+
+kubectl rollout status $DEPLOYMENT -n $PRINTNANNY_NAMESPACE --wait --timeout=20m
