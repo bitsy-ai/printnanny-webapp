7c97bee9b98c0d508bd05c8d043c6bde45da3d6e
Leigh Johnson <leigh@bitsy.ai>
2023-04-12 18:30:28 -0700

    log chckout.session.* kwargs

--- print_nanny_webapp/shop/signals.py
+++ print_nanny_webapp/shop/signals.py
@@ -8,28 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 @webhooks.handler("checkout.session.completed")
-def notify_checkout_session_completed(event, **_kwargs):
-    # session_id = event.data.get("id")
-    # if session_id is None:
-    #     logger.error("Failed to read event.id from event=%s", event)
-    #     return
-
-    # session = models.Session(id=session_id)
-    # dashboard_url = session.get_stripe_dashboard_url()
-
+def notify_checkout_session_completed(event, **kwargs):
+    logger.info("checkout.session.completed event=%s kwargs=%s", event, kwargs)
     body = {"content": f"Stripe checkout.session.completed - {event}"}
     requests.post(settings.DISCORD_NEW_SIGNUP_WEBHOOK, json=body, timeout=30)
 
 
 @webhooks.handler("checkout.session.expired")
-def notify_checkout_session_expired(event, **_kwargs):
-    # session_id = event.data.get("id")
-    # if session_id is None:
-    #     logger.error("Failed to read event.id from event=%s", event)
-    #     return
-
-    # session = models.Session(id=session_id)
-    # dashboard_url = session.get_stripe_dashboard_url()
-
+def notify_checkout_session_expired(event, **kwargs):
+    logger.info("checkout.session.expired event=%s kwargs=%s", event, kwargs)
     body = {"content": f"Stripe checkout.session.expired- {event}"}
     requests.post(settings.DISCORD_NEW_SIGNUP_WEBHOOK, json=body, timeout=30)
