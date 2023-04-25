7cb0094f718adb9d727f7e5f01ae06d0abbc436c
Leigh Johnson <leigh@bitsy.ai>
2023-04-25 08:50:00 -0700

    tweak copy

--- ui/src/components/shop/PriceTableEnterprise.vue
+++ ui/src/components/shop/PriceTableEnterprise.vue
@@ -179,7 +179,7 @@
         <button
           class="w-full md:w-1/3 m-auto transform md-shadow hover:scale-110 ease-in-out delay-150 duration-300 mt-6 block w-full sm:text-xl lg:text-lg xl:text-xl py-3 px-4 rounded-md shadow bg-gradient-to-r rounded-md shadow bg-gradient-to-r from-indigo-500 to-violet-600 text-white font-medium hover:from-indigo-600 hover:to-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
         >
-          Try Starter Plan for $9.99/month
+          Try the Starter Plah
         </button>
       </router-link>
     </h1>
