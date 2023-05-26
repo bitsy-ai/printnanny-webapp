d47b18b491a9653d5a9cb190f3fc859620e18ee7
Leigh Johnson <leigh@bitsy.ai>
2023-05-26 11:25:00 -0700

    create a default workspace when user account is created

--- print_nanny_webapp/workspaces/services.py
+++ print_nanny_webapp/workspaces/services.py
@@ -1,4 +1,5 @@
 from django.db.models.query import QuerySet
+from django.utils.text import slugify
 from rest_framework.exceptions import PermissionDenied
 
 from print_nanny_webapp.workspaces.models import (
@@ -6,6 +7,18 @@ from print_nanny_webapp.workspaces.models import (
     WorkspaceUser,
     WorkspaceInvitation,
 )
+
+
+def default_workspace_name(user) -> str:
+    if (
+        user.first_name is None
+        or user.last_name is None
+        or user.first_name == ""
+        or user.last_name == ""
+    ):
+        return f"{user.email}'s Workspace"
+    else:
+        return f"{user.first_name} {user.last_name}'s Workspace"
 
 
 def get_workspaces_by_auth_user(user) -> QuerySet[Workspace]:
@@ -18,3 +31,13 @@ def verify_workspace_invite(token: str, email: str):
     if invite.invitee_identifier == email:
         return invite
     raise PermissionDenied()
+
+
+def create_personal_workspace(user) -> Workspace:
+    workspace_name = default_workspace_name(user)
+    workspace_slug = slugify(workspace_name)
+    workspace = Workspace.objects.create(
+        slug=workspace_slug, name=workspace_name, is_active=True
+    )
+    workspace.get_or_add_user(user)
+    return workspace
